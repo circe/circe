c72dc5b6c898b3a8477160d128c7a2427bf701ba
Travis Brown <tbrown@twitter.com>
2015-08-10 15:37:45 -0500

    Add support for patching case classes with JSON

---
+++ generic/src/main/scala/io/circe/generic/PatchWithOptions.scala
@@ -0,0 +1,34 @@
+package io.circe.generic
+
+import shapeless._
+import shapeless.labelled.{ FieldType, field }
+
+trait PatchWithOptions[R <: HList] {
+  type Out <: HList
+
+  def apply(r: R, o: Out): R
+}
+
+object PatchWithOptions {
+  type Aux[R <: HList, Out0 <: HList] = PatchWithOptions[R] { type Out = Out0 }
+
+  implicit val hnilPatchWithOptions: Aux[HNil, HNil] =
+    new PatchWithOptions[HNil] {
+      type Out = HNil
+
+      def apply(r: HNil, o: HNil): HNil = HNil
+    }
+
+  implicit def hconsPatchWithOptions[K <: Symbol, V, T <: HList](implicit
+    tailPatch: PatchWithOptions[T]
+  ): Aux[FieldType[K, V] :: T, FieldType[K, Option[V]] :: tailPatch.Out] =
+    new PatchWithOptions[FieldType[K, V] :: T] {
+      type Out = FieldType[K, Option[V]] :: tailPatch.Out
+
+      def apply(
+        r: FieldType[K, V] :: T,
+        o: FieldType[K, Option[V]] :: tailPatch.Out
+      ): FieldType[K, V] :: T =
+        field[K](o.head.getOrElse(r.head)) :: tailPatch(r.tail, o.tail)
+    }
+}
