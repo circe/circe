2fdc252a71737a2f4e29f8d59abbd6043f40330d
Travis Brown <travisrobertbrown@gmail.com>
2015-10-29 10:56:48 -0400

    Use new re-exportation annotation

--- generic/shared/src/main/scala/io/circe/generic/auto.scala
+++ generic/shared/src/main/scala/io/circe/generic/auto.scala
@@ -1,10 +1,8 @@
 package io.circe.generic
 
-import export.Export
-import io.circe.{ Decoder, ObjectEncoder }
+import export.reexports
 import io.circe.generic.decoding.DerivedDecoder
 import io.circe.generic.encoding.DerivedObjectEncoder
-import shapeless.Lazy
 
 /**
  * Fully automatic codec derivation.
@@ -13,24 +11,5 @@ import shapeless.Lazy
  * instances for tuples, case classes (if all members have instances), "incomplete" case classes,
  * sealed trait hierarchies, etc.
  */
-package object auto {
-  /**
-   * Causes the `RecursiveAdtExample` tests in `auto` not to compile.
-   *
-  implicit def decoderExports[D[x] >: DerivedDecoder[x], T](implicit
-  	decode: Lazy[DerivedDecoder[T]]
-  ): Export[D[T]] = DerivedDecoder.exports[D, T]
-
-  implicit def objectEncoderExports[E[x] >: DerivedObjectEncoder[x], T](implicit
-    encode: Lazy[DerivedObjectEncoder[T]]
-  ): Export[E[T]] = DerivedObjectEncoder.exports[E, T]
-  */
-
-  import scala.language.experimental.macros
-
-  implicit def decoderExports[D[x] >: DerivedDecoder[x], T]: Export[D[T]] =
-    macro export.ExportMacro.exportsImpl0[DerivedDecoder, T, Export]
-
-  implicit def objectEncoderExports[E[x] >: DerivedObjectEncoder[x], T]: Export[E[T]] =
-    macro export.ExportMacro.exportsImpl0[DerivedObjectEncoder, T, Export]
-}
+@reexports[DerivedDecoder, DerivedObjectEncoder]
+object auto
