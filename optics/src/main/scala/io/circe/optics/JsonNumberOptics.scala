577023f73213247ded202dd20929fba49bfd2223
Travis Brown <travisrobertbrown@gmail.com>
2015-10-30 12:26:14 -0400

    Experimenting with optics

--- optics/src/main/scala/io/circe/optics/JsonNumberOptics.scala
+++ optics/src/main/scala/io/circe/optics/JsonNumberOptics.scala
@@ -11,28 +11,30 @@ import monocle.{ Iso, Prism }
  * @author Travis Brown
  */
 trait JsonNumberOptics {
-  val jsonNumberToDouble: Prism[JsonNumber, Double] = Prism[JsonNumber, Double]{ n =>
+  lazy val jsonNumberToDouble: Prism[JsonNumber, Double] = Prism[JsonNumber, Double]{ n =>
     val d = n.toDouble
     if (JsonNumber.eqJsonNumber.eqv(JsonDouble(d), n)) Some(d) else None
   }(JsonDouble(_))
 
-  val jsonNumberToBigInt: Prism[JsonNumber, BigInt] =
+  lazy val jsonNumberToBigInt: Prism[JsonNumber, BigInt] =
     Prism[JsonNumber, BigInt](_.toBigInt)(b =>
       JsonBigDecimal(BigDecimal(b, MathContext.UNLIMITED))
     )
 
-  val jsonNumberToLong: Prism[JsonNumber, Long] =
+  lazy val jsonNumberToLong: Prism[JsonNumber, Long] =
     Prism[JsonNumber, Long](_.toLong)(JsonLong(_))
 
-  val jsonNumberToInt: Prism[JsonNumber, Int] =
+  lazy val jsonNumberToInt: Prism[JsonNumber, Int] =
     Prism[JsonNumber, Int](_.toInt)(i => JsonLong(i.toLong))
 
-  val jsonNumberToShort: Prism[JsonNumber, Short] =
+  lazy val jsonNumberToShort: Prism[JsonNumber, Short] =
     Prism[JsonNumber, Short](_.toShort)(s => JsonLong(s.toLong))
 
-  val jsonNumberToByte: Prism[JsonNumber, Byte] =
+  lazy val jsonNumberToByte: Prism[JsonNumber, Byte] =
     Prism[JsonNumber, Byte](_.toByte)(b => JsonLong(b.toLong))
 
-  val jsonNumberToBigDecimal: Iso[JsonNumber, BigDecimal] =
+  lazy val jsonNumberToBigDecimal: Iso[JsonNumber, BigDecimal] =
     Iso[JsonNumber, BigDecimal](_.toBigDecimal)(JsonBigDecimal(_))
 }
+
+object JsonNumberOptics extends JsonNumberOptics
