737c7ea33ccf3746a262bffba17bf389ba35faa0
Travis Brown <travisrobertbrown@gmail.com>
2015-11-14 15:43:13 -0500

    Fix concurrency bug in default printers

---
+++ tests/shared/src/test/scala/io/circe/MemoizedPiecesSuite.scala
@@ -0,0 +1,46 @@
+package io.circe
+
+import io.circe.tests.CirceSuite
+import org.scalacheck.{ Arbitrary, Gen }
+import org.scalatest.concurrent.ScalaFutures
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class MemoizedPiecesSuite extends CirceSuite with ScalaFutures {
+  case class Depths(depths: Seq[Int])
+
+  object Depths {
+    implicit val arbitraryDepths: Arbitrary[Depths] = Arbitrary(
+      Gen.containerOfN[Seq, Int](512, Gen.choose(0, 256)).map(Depths(_))
+    )
+  }
+
+  def makePieces: Printer.MemoizedPieces = new Printer.MemoizedPieces {
+    def compute(i: Int): Printer.Pieces = Printer.Pieces(
+      "%s%s%s".format(" " * i, "a", " " * (i + 1)),
+      "%s%s%s".format(" " * i, "b", " " * (i + 1)),
+      "%s%s%s".format(" " * i, "c", " " * (i + 1)),
+      "%s%s%s".format(" " * i, "d", " " * (i + 1)),
+      "%s%s%s".format(" " * i, "e", " " * (i + 1)),
+      "%s%s%s".format(" " * i, "f", " " * (i + 1)),
+      "%s%s%s".format(" " * i, "g", " " * (i + 1)),
+      "%s%s%s".format(" " * i, "h", " " * (i + 1))
+    )
+  }
+
+  val pieces: Printer.MemoizedPieces = {
+    val tmp = makePieces
+    makePieces(256)
+    tmp
+  }
+
+  test("Concurrent memoized usage should be correct for arbitrarily ordered depths") {
+    check { depths: Depths =>
+      val newPieces = makePieces
+
+      whenReady(Future.traverse(depths.depths)(depth => Future(newPieces(depth)))) { result =>
+        result.sameElements(depths.depths.map(pieces(_)))
+      }
+    }
+  }
+}
