968747030451d9d3f668e14914c0a26d1a09fea1
Travis Brown <travisrobertbrown@gmail.com>
2015-11-25 13:20:48 -0600

    Add non-compilation tests for bad instances

--- generic/shared/src/main/scala/io/circe/generic/auto.scala
+++ generic/shared/src/main/scala/io/circe/generic/auto.scala
@@ -13,17 +13,4 @@ import io.circe.generic.encoding.DerivedObjectEncoder
  * sealed trait hierarchies, etc.
  */
 @reexports[DerivedDecoder, DerivedObjectEncoder]
-object auto {
-  /**
-   * Blocks derivation until Shapeless #453 is fixed.
-   */
-  implicit def decodeObject0: Decoder[Object] = sys.error("No Decoder[Object]")
-  implicit def decodeObject1: Decoder[Object] = sys.error("No Decoder[Object]")
-  implicit def decodeAnyRef0: Decoder[AnyRef] = sys.error("No Decoder[AnyRef]")
-  implicit def decodeAnyRef1: Decoder[AnyRef] = sys.error("No Decoder[AnyRef]")
-
-  implicit def encodeObject0: Encoder[Object] = sys.error("No Encoder[Object]")
-  implicit def encodeObject1: Encoder[Object] = sys.error("No Encoder[Object]")
-  implicit def encodeAnyRef0: Encoder[AnyRef] = sys.error("No Encoder[AnyRef]")
-  implicit def encodeAnyRef1: Encoder[AnyRef] = sys.error("No Encoder[AnyRef]")
-}
+object auto
