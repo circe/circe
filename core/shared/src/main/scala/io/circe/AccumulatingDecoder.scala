fe2d09f6987ba81c06e9bc0096bac8bfeccf6d98
Travis Brown <travisrobertbrown@gmail.com>
2015-12-12 13:14:26 +0000

    Add applicative instance for accumulating decoder

--- core/shared/src/main/scala/io/circe/AccumulatingDecoder.scala
+++ core/shared/src/main/scala/io/circe/AccumulatingDecoder.scala
@@ -1,6 +1,8 @@
 package io.circe
 
-import cats.data.ValidatedNel
+import cats.Applicative
+import cats.data.{ Validated, ValidatedNel }
+import cats.std.list._
 
 sealed trait AccumulatingDecoder[A] extends Serializable { self =>
   /**
@@ -14,6 +16,10 @@ sealed trait AccumulatingDecoder[A] extends Serializable { self =>
   def map[B](f: A => B): AccumulatingDecoder[B] = new AccumulatingDecoder[B] {
     def apply(c: HCursor): AccumulatingDecoder.Result[B] = self(c).map(f)
   }
+
+  def ap[B](f: AccumulatingDecoder[A => B]): AccumulatingDecoder[B] = new AccumulatingDecoder[B] {
+    def apply(c: HCursor): AccumulatingDecoder.Result[B] = self(c).ap(f(c))
+  }
 }
 
 object AccumulatingDecoder {
@@ -24,10 +30,15 @@ object AccumulatingDecoder {
       def apply(c: HCursor): Result[A] = decode.decodeAccumulating(c)
     }
 
-  /*implicit val accumulatingDecoderApplicative: Applicative[AccumulatingDecoder] =
+  implicit val accumulatingDecoderApplicative: Applicative[AccumulatingDecoder] =
     new Applicative[AccumulatingDecoder] {
-      def pure[A](a: A): AccumulatingDecoder[A] = instance(_ => Xor.right(a))
-      def flatMap[A, B](fa: AccumulatingDecoder[A])(f: A => Decoder[B]): Decoder[B] = fa.flatMap(f)
+      def pure[A](a: A): AccumulatingDecoder[A] = new AccumulatingDecoder[A] {
+        def apply(c: HCursor): AccumulatingDecoder.Result[A] = Validated.valid(a)
+      }
+      override def map[A, B](fa: AccumulatingDecoder[A])(f: A => B): AccumulatingDecoder[B] =
+        fa.map(f)
+      def ap[A, B](fa: AccumulatingDecoder[A])(
+        f: AccumulatingDecoder[A => B]
+      ): AccumulatingDecoder[B] = fa.ap(f)
     }
-  }*/
 }
