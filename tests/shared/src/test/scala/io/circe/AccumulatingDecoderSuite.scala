60ad8ebe772f06771d4d7d8e0eefd46a1cd97ad6
Travis Brown <travisrobertbrown@gmail.com>
2015-12-15 19:40:11 +0000

    Fix tests for accumulating decoders

--- tests/shared/src/test/scala/io/circe/AccumulatingDecoderSuite.scala
+++ tests/shared/src/test/scala/io/circe/AccumulatingDecoderSuite.scala
@@ -1,65 +1,63 @@
 package io.circe
 
 import cats.data.NonEmptyList
-import io.circe.AccumulatingDecoder.Result
-import io.circe.tests.CirceSuite
 import io.circe.generic.auto._
 import io.circe.syntax._
-
+import io.circe.tests.CirceSuite
 
 class AccumulatingDecoderSuite extends CirceSuite {
 
-  test("Accumulating decoder returns as many errors as invalid elements in a List") {
+  test("Accumulating decoder returns as many errors as invalid elements in a list") {
     check { (xs: List[Either[String, Int]]) =>
       val json = xs.map(_.fold(Json.string, Json.int)).asJson
-      val decoded: Result[List[Int]] = Decoder[List[Int]].accumulating(json.hcursor)
-      val stringElems = xs.collect { case elem if elem.isLeft => elem.left.get }
+      val decoded = Decoder[List[String]].accumulating(json.hcursor)
+      val intElems = xs.collect { case Right(elem) => elem }
 
-      decoded.fold(nel => nel.tail.size + 1, _ => 0) === stringElems.size
+      decoded.fold(_.tail.size + 1, _ => 0) === intElems.size
     }
   }
 
-  test("Accumulating decoder returns as many errors as invalid elements in a Map") {
+  test("Accumulating decoder returns as many errors as invalid elements in a map") {
     check { (xs: Map[String, Either[String, Int]]) =>
       val json = xs.map { case (k, v) => (k, v.fold(Json.string, Json.int)) }.asJson
-      val decoded = Decoder[Map[String, Int]].accumulating(json.hcursor)
-      val stringElems = xs.collect { case (k, elem) if elem.isLeft => elem.left.get }
+      val decoded = Decoder[Map[String, String]].accumulating(json.hcursor)
+      val intElems = xs.values.collect { case Right(elem) => elem }
 
-      decoded.fold(nel => nel.tail.size + 1, _ => 0) === stringElems.size
+      decoded.fold(_.tail.size + 1, _ => 0) === intElems.size
     }
   }
 
-  test("Accumulating decoder returns as many errors as invalid elements in a Set") {
+  test("Accumulating decoder returns as many errors as invalid elements in a set") {
     check { (xs: Set[Either[String, Int]]) =>
       val json = xs.map(_.fold(Json.string, Json.int)).asJson
-      val decoded = Decoder[Set[Int]].accumulating(json.hcursor)
-      val stringElems = xs.collect { case elem if elem.isLeft => elem.left.get }
+      val decoded = Decoder[Set[String]].accumulating(json.hcursor)
+      val intElems = xs.collect { case Right(elem) => elem }
 
-      decoded.fold(nel => nel.tail.size + 1, _ => 0) === stringElems.size
+      decoded.fold(_.tail.size + 1, _ => 0) === intElems.size
     }
   }
 
-  test("Accumulating decoder returns as many errors as invalid elements in a Tuple") {
+  test("Accumulating decoder returns as many errors as invalid elements in a tuple") {
     check { (xs: (Either[String, Int], Either[String, Int], Either[String, Int])) =>
-      val json = (xs._1.fold(Json.string, Json.int),
+      val json = (
+        xs._1.fold(Json.string, Json.int),
         xs._2.fold(Json.string, Json.int),
-        xs._3.fold(Json.string, Json.int)).asJson
-      val decoded = Decoder[(Int, Int, Int)].accumulating(json.hcursor)
-      val stringElems = xs.productIterator
-        .map(_.asInstanceOf[Either[String, Int]])
-        .collect { case elem if elem.isLeft => elem.left.get }
+        xs._3.fold(Json.string, Json.int)
+      ).asJson
+      val decoded = Decoder[(String, String, String)].accumulating(json.hcursor)
+      val intElems = List(xs._1, xs._2, xs._3).collect { case Right(elem) => elem }
 
-      decoded.fold(nel => nel.tail.size + 1, _ => 0) === stringElems.size
+      decoded.fold(_.tail.size + 1, _ => 0) === intElems.size
     }
   }
 
-  test("Accumulating decoder returns as many errors as invalid elements in a NonEmptyList") {
+  test("Accumulating decoder returns as many errors as invalid elements in a non-empty list") {
     check { (nel: NonEmptyList[Either[String, Int]]) =>
       val json = nel.map(_.fold(Json.string, Json.int)).asJson
-      val decoded = Decoder[NonEmptyList[Int]].accumulating(json.hcursor)
-      val stringElems = nel.toList.collect { case elem if elem.isLeft => elem.left.get }
+      val decoded = Decoder[NonEmptyList[String]].accumulating(json.hcursor)
+      val intElems = nel.toList.collect { case Right(elem) => elem }
 
-      decoded.fold(err => err.tail.size + 1, _ => 0) === stringElems.size
+      decoded.fold(_.tail.size + 1, _ => 0) === intElems.size
     }
   }
 
@@ -71,12 +69,12 @@ class AccumulatingDecoderSuite extends CirceSuite {
       val json = new Sample(a, b, c).asJson
       val decoded = Decoder[BadSample].accumulating(json.hcursor)
 
-      decoded.fold(err => err.tail.size + 1, _ => 0) === 3
+      decoded.fold(_.tail.size + 1, _ => 0) === 3
     }
   }
 
   //TODO: add tests replaying cursor history to ensure it is correct (next push)
-  ignore("Accumulating decoder error messages name all the invalid elements in a Json Array") {
+  ignore("Accumulating decoder error messages name all the invalid elements in a JSON array") {
     fail
   }
 }
