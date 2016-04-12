e2bfeb20f32d29ff2a0aca3e5f858b6a7d225e82
Rahul Singhai <Rahul.Singhai@barclayscorp.com>
2016-04-12 15:33:15 +0100

    Added extra parameter of type LocalDateTime to showcase its encoding-decoding

--- examples/todo-spray/src/main/scala/todo/TodoService.scala
+++ examples/todo-spray/src/main/scala/todo/TodoService.scala
@@ -1,23 +1,27 @@
 package todo
 
-import akka.actor.{ Actor, ActorRef, ActorRefFactory, ActorSystem, Props }
+import java.time.LocalDateTime
+
+import akka.actor.{Actor, ActorRef, ActorRefFactory, ActorSystem, Props}
 import akka.event.Logging
 import akka.io.IO
 import akka.pattern.ask
 import akka.util.Timeout
 import cats.syntax.show._
-import io.circe.{ DecodingFailure, Errors }
+import io.circe.{Decoder, DecodingFailure, Encoder, Errors}
 import io.circe.generic.auto._
 import io.circe.spray.ErrorAccumulatingJsonSupport._
+import io.circe.syntax._
 import java.util.UUID
+
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import spray.can.Http
 import spray.http.StatusCodes.BadRequest
 import spray.routing.Directives._
-import spray.routing.{ HttpService, MalformedRequestContentRejection, RejectionHandler, Route }
+import spray.routing.{HttpService, MalformedRequestContentRejection, RejectionHandler, Route}
 
-case class Todo(id: UUID, title: String, completed: Boolean, order: Int)
+case class Todo(id: UUID, title: String, completed: Boolean, order: Int, dueDate: LocalDateTime)
 
 object Boot extends App {
   implicit val system: ActorSystem = ActorSystem("todo-service")
@@ -33,6 +37,9 @@ object Boot extends App {
 
 class TodoService extends Actor with HttpService {
   def actorRefFactory: ActorRefFactory = context
+
+  implicit val dateTimeEncoder: Encoder[LocalDateTime] = Encoder.instance(a => a.toString.asJson)
+  implicit val dateTimeDecoder: Decoder[LocalDateTime] = Decoder.instance(a => a.as[String].map(LocalDateTime.parse(_)))
 
   val rejectionHandler: RejectionHandler = RejectionHandler {
     case MalformedRequestContentRejection(_, Some(errors @ Errors(_))) :: _ =>
