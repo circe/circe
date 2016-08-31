21507a8f6a17cc3885ecfe855c7eb30d0bc9c325
Travis Brown <travisrobertbrown@gmail.com>
2016-08-31 12:31:14 -0500

    Update iteratee.io to 0.6.0 release

--- tests/jvm/src/test/scala/io/circe/streaming/StreamingSuite.scala
+++ tests/jvm/src/test/scala/io/circe/streaming/StreamingSuite.scala
@@ -1,22 +1,22 @@
 package io.circe.streaming
 
 import cats.Eval
-import cats.data.{ Xor, XorT }
+import cats.data.EitherT
 import io.circe.generic.auto._
 import io.circe.syntax._
 import io.circe.tests.CirceSuite
 import io.circe.tests.examples._
 import io.iteratee.{ Enumeratee, Enumerator }
-import io.iteratee.xor._
+import io.iteratee.modules.eitherT._
 
 class StreamingSuite extends CirceSuite {
-  type Result[A] = XorT[Eval, Throwable, A]
+  type Result[A] = EitherT[Eval, Throwable, A]
 
   def enumerateFoos(fooStream: Stream[Foo], fooVector: Vector[Foo]): Enumerator[Result, Foo] =
     enumStream(fooStream).append(enumVector(fooVector))
 
   def serializeFoos(foos: Enumerator[Result, Foo]): Enumerator[Result, String] =
-    enumOne("[").append(foos.mapE(map((_: Foo).asJson.spaces2).andThen(intersperse(", ")))).append(enumOne("]"))
+    enumOne("[").append(foos.through(map((_: Foo).asJson.spaces2).andThen(intersperse(", ")))).append(enumOne("]"))
 
   def stringToBytes: Enumeratee[Result, String, Array[Byte]] = map(
     _.getBytes(java.nio.charset.Charset.forName("UTF-8"))
@@ -26,20 +26,20 @@ class StreamingSuite extends CirceSuite {
     val enumerator = serializeFoos(enumerateFoos(fooStream, fooVector))
     val foos = (fooStream ++ fooVector).map(_.asJson)
 
-    assert(enumerator.mapE(stringParser).toVector.value.value === Xor.right(foos.toVector))
+    assert(enumerator.through(stringParser).toVector.value.value === Right(foos.toVector))
   }
 
   "byteParser" should "parse enumerated bytes" in forAll { (fooStream: Stream[Foo], fooVector: Vector[Foo]) =>
-    val enumerator = serializeFoos(enumerateFoos(fooStream, fooVector)).mapE(stringToBytes)
+    val enumerator = serializeFoos(enumerateFoos(fooStream, fooVector)).through(stringToBytes)
     val foos = (fooStream ++ fooVector).map(_.asJson)
 
-    assert(enumerator.mapE(byteParser).toVector.value.value === Xor.right(foos.toVector))
+    assert(enumerator.through(byteParser).toVector.value.value === Right(foos.toVector))
   }
 
   "decoder" should "decode enumerated JSON values" in forAll { (fooStream: Stream[Foo], fooVector: Vector[Foo]) =>
     val enumerator = serializeFoos(enumerateFoos(fooStream, fooVector))
     val foos = fooStream ++ fooVector
 
-    assert(enumerator.mapE(stringParser).mapE(decoder[Result, Foo]).toVector.value.value === Xor.right(foos.toVector))
+    assert(enumerator.through(stringParser).through(decoder[Result, Foo]).toVector.value.value === Right(foos.toVector))
   }
 }
