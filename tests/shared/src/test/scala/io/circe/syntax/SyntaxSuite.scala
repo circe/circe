302349fdd46f495e21c67097620c48896f89830d
Travis Brown <travisrobertbrown@gmail.com>
2016-08-31 12:42:08 -0500

    Add asJsonObject to syntax

--- tests/shared/src/test/scala/io/circe/syntax/SyntaxSuite.scala
+++ tests/shared/src/test/scala/io/circe/syntax/SyntaxSuite.scala
@@ -1,10 +1,14 @@
 package io.circe.syntax
 
-import io.circe.Json
+import io.circe.{ Encoder, Json }
 import io.circe.tests.CirceSuite
 
 class SyntaxSuite extends CirceSuite {
   "asJson" should "be available and work appropriately" in forAll { (s: String) =>
     assert(s.asJson === Json.fromString(s))
+  }
+
+  "asJsonObject" should "be available and work appropriately" in forAll { (m: Map[String, Int]) =>
+    assert(m.asJsonObject === Encoder[Map[String, Int]].apply(m).asObject.get)
   }
 }
