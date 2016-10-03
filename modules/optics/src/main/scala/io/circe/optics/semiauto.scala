477318fa6e5b509bac8623d02cec2964ed7486b3
André L. F. Pinto <andreluizferreirapinto@gmail.com>
2016-10-02 22:06:39 -0300

    Reformat long lines

--- modules/optics/src/main/scala/io/circe/optics/semiauto.scala
+++ modules/optics/src/main/scala/io/circe/optics/semiauto.scala
@@ -10,11 +10,15 @@ import monocle.Iso
  * instances when there already is Decoder[A]/Encoder[A] and Iso[A, B] or Iso[B, A].
  */
 final object semiauto {
-  final def deriveDecoderWithIso[A, B](implicit decoder: Decoder[A], iso: Iso[A, B]): Decoder[B] = decoder.map(iso.get)
+  final def deriveDecoderWithIso[A, B](implicit decoder: Decoder[A], iso: Iso[A, B]): Decoder[B] =
+    decoder.map(iso.get)
 
-  final def deriveEncoderWithIso[A, B](implicit encoder: Encoder[A], iso: Iso[A, B]): Encoder[B] = encoder.contramap(iso.reverseGet)
+  final def deriveEncoderWithIso[A, B](implicit encoder: Encoder[A], iso: Iso[A, B]): Encoder[B] =
+    encoder.contramap(iso.reverseGet)
 
-  final def deriveDecoderWithIso[B, A](implicit decoder: Decoder[A], iso: Iso[B, A], e: DummyImplicit): Decoder[B] = decoder.map(iso.reverseGet)
+  final def deriveDecoderWithIso[B, A](implicit decoder: Decoder[A], iso: Iso[B, A], e: DummyImplicit): Decoder[B] =
+    decoder.map(iso.reverseGet)
 
-  final def deriveEncoderWithIso[B, A](implicit encoder: Encoder[A], iso: Iso[B, A], e: DummyImplicit): Encoder[B] = encoder.contramap(iso.get)
+  final def deriveEncoderWithIso[B, A](implicit encoder: Encoder[A], iso: Iso[B, A], e: DummyImplicit): Encoder[B] =
+    encoder.contramap(iso.get)
 }
