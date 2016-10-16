a7bf59aa533fb17c9dd274a604d3f4b8a28d0fc1
Travis Brown <travisrobertbrown@gmail.com>
2016-10-16 15:14:01 -0500

    Add Jackson-specific test instances

--- modules/jackson/src/main/scala/io/circe/jackson/package.scala
+++ modules/jackson/src/main/scala/io/circe/jackson/package.scala
@@ -6,6 +6,12 @@ import com.fasterxml.jackson.databind.ObjectWriter
 /**
  * Support for Jackson-powered parsing and printing for circe.
  *
+ * Note that not all guarantees that hold for Jawn-based parsing and the default
+ * printer will hold for the Jackson-based versions. Jackson's handling of
+ * numbers in particular differs significantly: it doesn't distinguish positive
+ * and negative zeros, it may truncate large JSON numbers or simply fail to
+ * parse them, it may print large numbers as strings, etc.
+ *
  * The implementation is ported with minimal changes from Play JSON.
  */
 package object jackson extends WithJacksonMapper with JacksonParser {
---
+++ modules/tests/jvm/src/test/scala/io/circe/jackson/JacksonInstances.scala
@@ -0,0 +1,65 @@
+package io.circe.jackson
+
+import cats.Eq
+import cats.instances.list._
+import cats.instances.map._
+import io.circe.{
+  Json,
+  JsonBigDecimal,
+  JsonBiggerDecimal,
+  JsonDecimal,
+  JsonDouble,
+  JsonLong,
+  JsonNumber
+}
+import io.circe.Json.{ JArray, JNumber, JObject, JString }
+import io.circe.numbers.BiggerDecimal
+import io.circe.testing.ArbitraryInstances
+import org.scalacheck.Arbitrary
+import scala.util.matching.Regex
+import scala.util.Try
+
+trait JacksonInstances { this: ArbitraryInstances =>
+  /**
+   * Jackson by default in some cases serializes numbers as strings, and we want
+   * to use a [[cats.Eq]] instance that takes that into account when testing.
+   */
+  implicit val eqJsonStringNumber: Eq[Json] = Eq.instance {
+    case ( JArray(a),  JArray(b)) => Eq[List[Json]].eqv(a.toList, b.toList)
+    case (JObject(a), JObject(b)) => Eq[Map[String, Json]].eqv(a.toMap, b.toMap)
+    case (JString(a), JNumber(b)) => a == b.toString
+    case (JNumber(a), JString(b)) => a.toString == b
+    case (a, b) => Json.eqJson.eqv(a, b)
+  }
+
+  private[this] val SigExpPattern: Regex = """[^eE]+[eE][+-]?(\d+)""".r
+  private[this] val replacement: JsonNumber = JsonBiggerDecimal(BiggerDecimal.fromLong(0L))
+
+  /**
+   * Jackson can't handle some very large numbers, so we replace them.
+   *
+   * Ideally it seems like we could set the cap at numbers with exponents larger
+   * than `Int.MaxValue`, but in practice that still results in
+   * `ArithmeticException`.
+   */
+  def cleanNumber(n: JsonNumber): JsonNumber = n.toString match {
+    case SigExpPattern(exp) if !Try(exp.toLong).toOption.exists(_ <= Short.MaxValue.toLong) => replacement
+    case _ => n match {
+      case v @ JsonDecimal(_) => cleanNumber(JsonBiggerDecimal(v.toBiggerDecimal))
+      case v @ JsonBiggerDecimal(value) =>
+        value.toBigDecimal.map(BigDecimal(_)).fold(replacement) { d =>
+          val fromBigDecimal = BiggerDecimal.fromBigDecimal(d.bigDecimal)
+
+          if (fromBigDecimal == value && d.abs <= BigDecimal(Double.MaxValue)) v else JsonBiggerDecimal(fromBigDecimal)
+        }
+      case v @ JsonBigDecimal(_) => v
+      case v @ JsonDouble(_) => v
+      case v @ JsonLong(_) => v
+    }
+  }
+
+  def cleanNumbers(json: Json): Json =
+    json.mapNumber(cleanNumber).mapArray(_.map(cleanNumbers)).mapObject(_.withJsons(cleanNumbers))
+
+  val arbitraryCleanedJson: Arbitrary[Json] = Arbitrary(Arbitrary.arbitrary[Json].map(cleanNumbers))
+}
