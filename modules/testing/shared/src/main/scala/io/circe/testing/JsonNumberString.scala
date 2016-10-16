4ec3295829621f2e78e5f61547775d4ba86c488a
Travis Brown <travisrobertbrown@gmail.com>
2016-10-16 14:05:45 -0500

    Miscellaneous minor formatting and documentation

--- modules/testing/shared/src/main/scala/io/circe/testing/JsonNumberString.scala
+++ modules/testing/shared/src/main/scala/io/circe/testing/JsonNumberString.scala
@@ -2,32 +2,34 @@ package io.circe.testing
 
 import org.scalacheck.{ Arbitrary, Gen }
 
-case class JsonNumberString(value: String)
+/**
+ * An arbitrary JSON number, represented as a string.
+ */
+final case class JsonNumberString(value: String)
 
-object JsonNumberString {
-  implicit val arbitraryJsonNumberString: Arbitrary[JsonNumberString] =
-    Arbitrary(
-      for {
-        sign <- Gen.oneOf("", "-")
-        integral <- Gen.oneOf(
-          Gen.const("0"),
-          for {
-            nonZero <- Gen.choose(1, 9).map(_.toString)
-            rest <- Gen.numStr
-          } yield s"$nonZero$rest"
-        )
-        fractional <- Gen.oneOf(
-          Gen.const(""),
-          Gen.nonEmptyListOf(Gen.numChar).map(_.mkString).map("." + _)
-        )
-        exponent <- Gen.oneOf(
-          Gen.const(""),
-          for {
-            e <- Gen.oneOf("e", "E")
-            s <- Gen.oneOf("", "+", "-")
-            n <- Gen.nonEmptyListOf(Gen.numChar).map(_.mkString)
-          } yield s"$e$s$n"
-        )
-      } yield JsonNumberString(s"$sign$integral$fractional$exponent")
-    )
+final object JsonNumberString {
+  implicit val arbitraryJsonNumberString: Arbitrary[JsonNumberString] = Arbitrary(
+    for {
+      sign <- Gen.oneOf("", "-")
+      integral <- Gen.oneOf(
+        Gen.const("0"),
+        for {
+          nonZero <- Gen.choose(1, 9).map(_.toString)
+          rest <- Gen.numStr
+        } yield s"$nonZero$rest"
+      )
+      fractional <- Gen.oneOf(
+        Gen.const(""),
+        Gen.nonEmptyListOf(Gen.numChar).map(_.mkString).map("." + _)
+      )
+      exponent <- Gen.oneOf(
+        Gen.const(""),
+        for {
+          e <- Gen.oneOf("e", "E")
+          s <- Gen.oneOf("", "+", "-")
+          n <- Gen.nonEmptyListOf(Gen.numChar).map(_.mkString)
+        } yield s"$e$s$n"
+      )
+    } yield JsonNumberString(s"$sign$integral$fractional$exponent")
+  )
 }
