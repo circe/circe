c3d73ca856ff81976d5ad6cf778e16ed03333463
Travis Brown <travisrobertbrown@gmail.com>
2016-11-20 14:15:40 -0600

    Miscellaneous clean-up

--- modules/core/shared/src/main/scala/io/circe/HCursor.scala
+++ modules/core/shared/src/main/scala/io/circe/HCursor.scala
@@ -22,40 +22,6 @@ sealed abstract class HCursor extends ACursor {
     case _ => None
   }
 
-  final def top: Option[Json] = {
-    @tailrec
-    def go(c: HCursor): Json = c match {
-      case v: HCursor.ValueCursor => v.value
-      case a: HCursor.ArrayCursor =>
-        val newValue = Json.fromValues((a.value :: a.rs).reverse_:::(a.ls))
-
-        go(
-          a.parent match {
-            case pv: HCursor.ValueCursor => pv.copy(value = newValue)
-            case pa: HCursor.ArrayCursor => pa.copy(value = newValue, history = Nil, changed = a.changed || pa.changed)
-            case po: HCursor.ObjectCursor => po.copy(
-              value = newValue,
-              history = Nil,
-              changed = a.changed || po.changed,
-              obj = if (a.changed) po.obj.add(po.key, newValue) else po.obj
-            )
-          }
-        )
-      case o: HCursor.ObjectCursor =>
-        val newValue = Json.fromJsonObject(if (o.changed) o.obj.add(o.key, o.value) else o.obj)
-
-        go(
-          o.parent match {
-            case pv: HCursor.ValueCursor => pv.copy(value = newValue)
-            case pa: HCursor.ArrayCursor => pa.copy(value = newValue, history = Nil, changed = o.changed || pa.changed)
-            case po: HCursor.ObjectCursor => po.copy(value = newValue, history = Nil, changed = o.changed || po.changed)
-          }
-        )
-    }
-
-    Some(go(this))
-  }
-
   final def leftN(n: Int): ACursor = if (n < 0) rightN(-n) else {
     @tailrec
     def go(i: Int, c: ACursor): ACursor = if (i == 0) c else go(i - 1, c.left)
@@ -92,25 +58,9 @@ sealed abstract class HCursor extends ACursor {
     go(this)
   }
 
-  final def downArray: ACursor = value match {
-    case Json.JArray(h :: t) =>
-      HCursor.ArrayCursor(h, HistoryOp.ok(CursorOp.DownArray) :: history, this, false, Nil, t)
-    case _ => fail(CursorOp.DownArray)
-  }
-
   final def downAt(p: Json => Boolean): ACursor = downArray.find(p)
 
   final def downN(n: Int): ACursor = downArray.rightN(n)
-
-  final def downField(k: String): ACursor = value match {
-    case Json.JObject(o) =>
-      val m = o.toMap
-
-      if (!m.contains(k)) fail(CursorOp.DownField(k)) else {
-        HCursor.ObjectCursor(m(k), HistoryOp.ok(CursorOp.DownField(k)) :: history, this, false, k, o)
-      }
-    case _ => fail(CursorOp.DownField(k))
-  }
 
   /**
    * Create a new cursor that has failed on the given operation.
@@ -127,53 +77,105 @@ sealed abstract class HCursor extends ACursor {
 final object HCursor {
   def fromJson(value: Json): HCursor = ValueCursor(value, Nil)
 
-  private[circe] final case class ValueCursor(value: Json, history: List[HistoryOp]) extends HCursor {
-    final def withFocus(f: Json => Json): ACursor = copy(value = f(value))
+  private[this] sealed abstract class BaseHCursor extends HCursor {
+    final def top: Option[Json] = {
+      @tailrec
+      def go(c: HCursor): Json = c match {
+        case v: ValueCursor => v.value
+        case a: ArrayCursor =>
+          val newValue = Json.fromValues((a.value :: a.rs).reverse_:::(a.ls))
 
-    final def withFocusM[F[_]](f: Json => F[Json])(implicit F: Applicative[F]): F[ACursor] =
-      F.map(f(value))(newValue => copy(value = newValue))
+          go(
+            a.parent match {
+              case pv: ValueCursor => pv.copy(value = newValue)
+              case pa: ArrayCursor => pa.copy(value = newValue, history = Nil, changed = a.changed || pa.changed)
+              case po: ObjectCursor => po.copy(
+                value = newValue,
+                history = Nil,
+                changed = a.changed || po.changed,
+                obj = if (a.changed) po.obj.add(po.key, newValue) else po.obj
+              )
+            }
+          )
+        case o: ObjectCursor =>
+          val newValue = Json.fromJsonObject(if (o.changed) o.obj.add(o.key, o.value) else o.obj)
 
-    final def lefts: Option[List[Json]] = None
-    final def rights: Option[List[Json]] = None
+          go(
+            o.parent match {
+              case pv: ValueCursor => pv.copy(value = newValue)
+              case pa: ArrayCursor => pa.copy(value = newValue, history = Nil, changed = o.changed || pa.changed)
+              case po: ObjectCursor => po.copy(value = newValue, history = Nil, changed = o.changed || po.changed)
+            }
+          )
+      }
 
-    final def up: ACursor = fail(CursorOp.MoveUp)
-    final def delete: ACursor = fail(CursorOp.DeleteGoParent)
+      Some(go(this))
+    }
 
-    final def left: ACursor = fail(CursorOp.MoveLeft)
-    final def right: ACursor = fail(CursorOp.MoveRight)
-    final def first: ACursor = fail(CursorOp.MoveFirst)
-    final def last: ACursor = fail(CursorOp.MoveLast)
+    final def downArray: ACursor = value match {
+      case Json.JArray(h :: t) =>
+        ArrayCursor(h, HistoryOp.ok(CursorOp.DownArray) :: history, this, false, Nil, t)
+      case _ => fail(CursorOp.DownArray)
+    }
 
-    final def deleteGoLeft: ACursor = fail(CursorOp.DeleteGoLeft)
-    final def deleteGoRight: ACursor = fail(CursorOp.DeleteGoRight)
-    final def deleteGoFirst: ACursor = fail(CursorOp.DeleteGoFirst)
-    final def deleteGoLast: ACursor = fail(CursorOp.DeleteGoLast)
-    final def deleteLefts: ACursor = fail(CursorOp.DeleteLefts)
-    final def deleteRights: ACursor = fail(CursorOp.DeleteRights)
+    final def downField(k: String): ACursor = value match {
+      case Json.JObject(o) =>
+        val m = o.toMap
 
-    final def setLefts(x: List[Json]): ACursor = fail(CursorOp.SetLefts(x))
-    final def setRights(x: List[Json]): ACursor = fail(CursorOp.SetRights(x))
-
-    final def field(k: String): ACursor = fail(CursorOp.Field(k))
-    final def deleteGoField(k: String): ACursor = fail(CursorOp.DeleteGoField(k))
-
-    final def reattempt: ACursor = ValueCursor(value, HistoryOp.reattempt :: history)
+        if (!m.contains(k)) fail(CursorOp.DownField(k)) else {
+          ObjectCursor(m(k), HistoryOp.ok(CursorOp.DownField(k)) :: history, this, false, k, o)
+        }
+      case _ => fail(CursorOp.DownField(k))
+    }
   }
 
-  private[circe] final case class ArrayCursor(
+  private[this] final case class ValueCursor(value: Json, history: List[HistoryOp]) extends BaseHCursor {
+    def withFocus(f: Json => Json): ACursor = copy(value = f(value))
+
+    def withFocusM[F[_]](f: Json => F[Json])(implicit F: Applicative[F]): F[ACursor] =
+      F.map(f(value))(newValue => copy(value = newValue))
+
+    def lefts: Option[List[Json]] = None
+    def rights: Option[List[Json]] = None
+
+    def up: ACursor = fail(CursorOp.MoveUp)
+    def delete: ACursor = fail(CursorOp.DeleteGoParent)
+
+    def left: ACursor = fail(CursorOp.MoveLeft)
+    def right: ACursor = fail(CursorOp.MoveRight)
+    def first: ACursor = fail(CursorOp.MoveFirst)
+    def last: ACursor = fail(CursorOp.MoveLast)
+
+    def deleteGoLeft: ACursor = fail(CursorOp.DeleteGoLeft)
+    def deleteGoRight: ACursor = fail(CursorOp.DeleteGoRight)
+    def deleteGoFirst: ACursor = fail(CursorOp.DeleteGoFirst)
+    def deleteGoLast: ACursor = fail(CursorOp.DeleteGoLast)
+    def deleteLefts: ACursor = fail(CursorOp.DeleteLefts)
+    def deleteRights: ACursor = fail(CursorOp.DeleteRights)
+
+    def setLefts(x: List[Json]): ACursor = fail(CursorOp.SetLefts(x))
+    def setRights(x: List[Json]): ACursor = fail(CursorOp.SetRights(x))
+
+    def field(k: String): ACursor = fail(CursorOp.Field(k))
+    def deleteGoField(k: String): ACursor = fail(CursorOp.DeleteGoField(k))
+
+    def reattempt: ACursor = ValueCursor(value, HistoryOp.reattempt :: history)
+  }
+
+  private[this] final case class ArrayCursor(
     value: Json,
     history: List[HistoryOp],
     parent: HCursor,
     changed: Boolean,
     ls: List[Json],
     rs: List[Json]
-  ) extends HCursor {
-    final def withFocus(f: Json => Json): ACursor = copy(value = f(value), changed = true)
+  ) extends BaseHCursor {
+    def withFocus(f: Json => Json): ACursor = copy(value = f(value), changed = true)
 
-    final def withFocusM[F[_]](f: Json => F[Json])(implicit F: Applicative[F]): F[ACursor] =
+    def withFocusM[F[_]](f: Json => F[Json])(implicit F: Applicative[F]): F[ACursor] =
       F.map(f(value))(newValue => copy(value = newValue, changed = true))
 
-    final def up: ACursor = {
+    def up: ACursor = {
       val newValue = Json.fromValues((value :: rs).reverse_:::(ls))
       val newHistory = HistoryOp.ok(CursorOp.MoveUp) :: history
 
@@ -274,21 +276,21 @@ final object HCursor {
     final def reattempt: ACursor = copy(history = HistoryOp.reattempt :: history)
   }
 
-  private[circe] final case class ObjectCursor(
+  private[this] final case class ObjectCursor(
     value: Json,
     history: List[HistoryOp],
     parent: HCursor,
     changed: Boolean,
     key: String,
     obj: JsonObject
-  ) extends HCursor {
-    final def withFocus(f: Json => Json): ACursor = copy(value = f(value), changed = true)
+  ) extends BaseHCursor {
+    def withFocus(f: Json => Json): ACursor = copy(value = f(value), changed = true)
 
-    final def withFocusM[F[_]](f: Json => F[Json])(implicit F: Applicative[F]): F[ACursor] =
+    def withFocusM[F[_]](f: Json => F[Json])(implicit F: Applicative[F]): F[ACursor] =
       F.map(f(value))(newValue => copy(value = newValue, changed = true))
 
-    final def lefts: Option[List[Json]] = None
-    final def rights: Option[List[Json]] = None
+    def lefts: Option[List[Json]] = None
+    def rights: Option[List[Json]] = None
 
     def up: ACursor = {
       val newValue = Json.fromJsonObject(if (changed) obj.add(key, value) else obj)
@@ -334,21 +336,21 @@ final object HCursor {
       ) else fail(CursorOp.DeleteGoField(k))
     }
 
-    final def left: ACursor = fail(CursorOp.MoveLeft)
-    final def right: ACursor = fail(CursorOp.MoveRight)
-    final def first: ACursor = fail(CursorOp.MoveFirst)
-    final def last: ACursor = fail(CursorOp.MoveLast)
+    def left: ACursor = fail(CursorOp.MoveLeft)
+    def right: ACursor = fail(CursorOp.MoveRight)
+    def first: ACursor = fail(CursorOp.MoveFirst)
+    def last: ACursor = fail(CursorOp.MoveLast)
 
-    final def deleteGoLeft: ACursor = fail(CursorOp.DeleteGoLeft)
-    final def deleteGoRight: ACursor = fail(CursorOp.DeleteGoRight)
-    final def deleteGoFirst: ACursor = fail(CursorOp.DeleteGoFirst)
-    final def deleteGoLast: ACursor = fail(CursorOp.DeleteGoLast)
-    final def deleteLefts: ACursor = fail(CursorOp.DeleteLefts)
-    final def deleteRights: ACursor = fail(CursorOp.DeleteRights)
+    def deleteGoLeft: ACursor = fail(CursorOp.DeleteGoLeft)
+    def deleteGoRight: ACursor = fail(CursorOp.DeleteGoRight)
+    def deleteGoFirst: ACursor = fail(CursorOp.DeleteGoFirst)
+    def deleteGoLast: ACursor = fail(CursorOp.DeleteGoLast)
+    def deleteLefts: ACursor = fail(CursorOp.DeleteLefts)
+    def deleteRights: ACursor = fail(CursorOp.DeleteRights)
 
-    final def setLefts(x: List[Json]): ACursor = fail(CursorOp.SetLefts(x))
-    final def setRights(x: List[Json]): ACursor = fail(CursorOp.SetRights(x))
+    def setLefts(x: List[Json]): ACursor = fail(CursorOp.SetLefts(x))
+    def setRights(x: List[Json]): ACursor = fail(CursorOp.SetRights(x))
 
-    final def reattempt: ACursor = copy(history = HistoryOp.reattempt :: history)
+    def reattempt: ACursor = copy(history = HistoryOp.reattempt :: history)
   }
 }
