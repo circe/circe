3eaa9eb55b2716fefe34888af5573c276f1e7252
Michal Sitko <pierwszy1@gmail.com>
2017-04-25 16:41:08 +0200

    551 BitVector encoder/decoder fix

--- modules/scodec/shared/src/main/scala/io/circe/scodec/package.scala
+++ modules/scodec/shared/src/main/scala/io/circe/scodec/package.scala
@@ -1,23 +1,65 @@
 package io.circe
 
-import _root_.scodec.bits.{BitVector, ByteVector}
+import _root_.scodec.bits.{ BitVector, ByteVector }
 
-package object scodec {
+private[circe] trait BitVectorCodec {
   implicit final val decodeBitVector: Decoder[BitVector] =
     Decoder.instance { c =>
-      Decoder.decodeString(c) match {
-        case Right(str) =>
-          BitVector.fromBase64Descriptive(str) match {
-            case r @ Right(_) => r.asInstanceOf[Decoder.Result[BitVector]]
-            case Left(err) => Left(DecodingFailure(err, c.history))
+      Decoder.decodeJsonObject(c) match {
+        case Right(jsonObject) =>
+          val decoded = for {
+            bitsJ     <- jsonObject("bits")
+            bits      <- bitsJ.asString
+            lengthJ   <- jsonObject("length")
+            length    <- lengthJ.asNumber.flatMap(_.toLong)
+            bitVector <- BitVector.fromBase64Descriptive(bits).right.toOption
+          } yield bitVector.take(length)
+
+          decoded match {
+            case Some(bitVector)  => Right(bitVector)
+            case None             => Left(DecodingFailure("Incorrect format of BitVector field", c.history))
           }
         case l @ Left(_) => l.asInstanceOf[Decoder.Result[BitVector]]
       }
     }
 
+  /**
+    * For serialization of BitVector we use base64. scodec's implementation of `toBase64` adds padding to 8 bits.
+    * That's not desired in our case and to preserve original BitVector length we add field "length".
+    *
+    * Examples:
+    * encodeBitVector(bin"101")
+    * res: io.circe.Json =
+    * {
+    *   "bits" : "oA==",
+    *   "length" : 3
+    * }
+    *
+    *
+    * encodeBitVector(bin"")
+    * res: io.circe.Json =
+    * {
+    *   "bits" : "",
+    *   "length" : 0
+    * }
+    *
+    * encodeBitVector(bin"11001100")
+    * res: io.circe.Json =
+    * {
+    *   "bits" : "zA==",
+    *   "length" : 8
+    * }
+    *
+    */
   implicit final val encodeBitVector: Encoder[BitVector] =
-    Encoder.encodeString.contramap(_.toBase64)
+    Encoder.encodeJsonObject.contramap { bv =>
+      JsonObject.empty
+        .add("bits", Json.fromString(bv.toBase64))
+        .add("length", Json.fromLong(bv.size))
+    }
+}
 
+private[circe] trait ByteVectorCodec {
   implicit final val decodeByteVector: Decoder[ByteVector] =
     Decoder.instance { c =>
       Decoder.decodeString(c) match {
@@ -33,3 +75,5 @@ package object scodec {
   implicit final val encodeByteVector: Encoder[ByteVector] =
     Encoder.encodeString.contramap(_.toBase64)
 }
+
+package object scodec extends ByteVectorCodec with BitVectorCodec
