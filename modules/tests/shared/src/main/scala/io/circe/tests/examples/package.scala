f7414c718423f4021fd68f406633c22eed66dedd
Travis Brown <travisrobertbrown@gmail.com>
2017-06-18 13:51:42 -0500

    Add instances for test example types

--- modules/tests/shared/src/main/scala/io/circe/tests/examples/package.scala
+++ modules/tests/shared/src/main/scala/io/circe/tests/examples/package.scala
@@ -2,7 +2,8 @@ package io.circe.tests
 
 import cats.Eq
 import cats.instances.AllInstances
-import io.circe.{ Decoder, Encoder, Json }
+import cats.syntax.functor._
+import io.circe.{ Decoder, DecodingFailure, Encoder, Json }
 import io.circe.testing.ArbitraryInstances
 import org.scalacheck.{ Arbitrary, Gen }
 
@@ -38,9 +39,7 @@ package examples {
 
   object Box {
     implicit def eqBox[A: Eq]: Eq[Box[A]] = Eq.by(_.a)
-
-    implicit def arbitraryBox[A](implicit A: Arbitrary[A]): Arbitrary[Box[A]] =
-      Arbitrary(A.arbitrary.map(Box(_)))
+    implicit def arbitraryBox[A](implicit A: Arbitrary[A]): Arbitrary[Box[A]] = Arbitrary(A.arbitrary.map(Box(_)))
   }
 
   case class Qux[A](i: Int, a: A, j: Int)
@@ -62,9 +61,10 @@ package examples {
 
   object Wub {
     implicit val eqWub: Eq[Wub] = Eq.by(_.x)
+    implicit val arbitraryWub: Arbitrary[Wub] = Arbitrary(Arbitrary.arbitrary[Long].map(Wub(_)))
 
-    implicit val arbitraryWub: Arbitrary[Wub] =
-      Arbitrary(Arbitrary.arbitrary[Long].map(Wub(_)))
+    val decodeWub: Decoder[Wub] = Decoder[Long].prepare(_.downField("x")).map(Wub(_))
+    val encodeWub: Encoder[Wub] = Encoder.instance(w => Json.obj("x" -> Json.fromLong(w.x)))
   }
 
   sealed trait Foo
@@ -72,11 +72,46 @@ package examples {
   case class Baz(xs: List[String]) extends Foo
   case class Bam(w: Wub, d: Double) extends Foo
 
+  object Bar {
+    implicit val eqBar: Eq[Bar] = Eq.fromUniversalEquals
+    implicit val arbitraryBar: Arbitrary[Bar] = Arbitrary(
+      for {
+       i <- Arbitrary.arbitrary[Int]
+        s <- Arbitrary.arbitrary[String]
+      } yield Bar(i, s)
+    )
+
+    val decodeBar: Decoder[Bar] = Decoder.forProduct2("i", "s")(Bar.apply)
+    val encodeBar: Encoder[Bar] = Encoder.forProduct2("i", "s") {
+      case Bar(i, s) => (i, s)
+    }
+  }
+
   object Baz {
+    implicit val eqBaz: Eq[Baz] = Eq.fromUniversalEquals
+    implicit val arbitraryBaz: Arbitrary[Baz] = Arbitrary(
+      Arbitrary.arbitrary[List[String]].map(Baz.apply)
+    )
+
     implicit val decodeBaz: Decoder[Baz] = Decoder[List[String]].map(Baz(_))
     implicit val encodeBaz: Encoder[Baz] = Encoder.instance {
       case Baz(xs) => Json.fromValues(xs.map(Json.fromString))
     }
+  }
+
+  object Bam {
+    implicit val eqBam: Eq[Bam] = Eq.fromUniversalEquals
+    implicit val arbitraryBam: Arbitrary[Bam] = Arbitrary(
+      for {
+        w <- Arbitrary.arbitrary[Wub]
+        d <- Arbitrary.arbitrary[Double]
+      } yield Bam(w, d)
+    )
+
+    val decodeBam: Decoder[Bam] = Decoder.forProduct2("w", "d")(Bam.apply)(Wub.decodeWub, implicitly)
+    val encodeBam: Encoder[Bam] = Encoder.forProduct2[Wub, Double, Bam]("w", "d") {
+      case Bam(w, d) => (w, d)
+    }(Wub.encodeWub, implicitly)
   }
 
   object Foo {
@@ -84,16 +119,25 @@ package examples {
 
     implicit val arbitraryFoo: Arbitrary[Foo] = Arbitrary(
       Gen.oneOf(
-        for {
-          i <- Arbitrary.arbitrary[Int]
-          s <- Arbitrary.arbitrary[String]
-        } yield Bar(i, s),
-        Gen.listOf(Arbitrary.arbitrary[String]).map(Baz.apply),
-        for {
-          w <- Arbitrary.arbitrary[Wub]
-          d <- Arbitrary.arbitrary[Double]
-        } yield Bam(w, d)
+        Arbitrary.arbitrary[Bar],
+        Arbitrary.arbitrary[Baz],
+        Arbitrary.arbitrary[Bam]
       )
     )
+
+    val encodeFoo: Encoder[Foo] = Encoder.instance {
+      case bar @ Bar(_, _) => Json.obj("Bar" -> Bar.encodeBar(bar))
+      case baz @ Baz(_) => Json.obj("Baz" -> Baz.encodeBaz(baz))
+      case bam @ Bam(_, _) => Json.obj("Bam" -> Bam.encodeBam(bam))
+    }
+
+    val decodeFoo: Decoder[Foo] = Decoder.instance { c =>
+      c.fields match {
+        case Some(Vector("Bar")) => c.get("Bar")(Bar.decodeBar.widen)
+        case Some(Vector("Baz")) => c.get("Baz")(Baz.decodeBaz.widen)
+        case Some(Vector("Bam")) => c.get("Bam")(Bam.decodeBam.widen)
+        case _ => Left(DecodingFailure("Foo", c.history))
+      }
+    }
   }
 }
