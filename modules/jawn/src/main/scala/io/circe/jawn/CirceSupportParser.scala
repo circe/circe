6de912f30239ec7f13b8937cb45258f51a9a643e
Travis Brown <travisbrown@stripe.com>
2017-07-02 19:21:37 -0500

    Use slices in jawn parsing

--- modules/jawn/src/main/scala/io/circe/jawn/CirceSupportParser.scala
+++ modules/jawn/src/main/scala/io/circe/jawn/CirceSupportParser.scala
@@ -4,18 +4,41 @@ import io.circe.{ Json, JsonBigDecimal, JsonBiggerDecimal, JsonObject }
 import io.circe.numbers.{ BiggerDecimal, JsonNumberParser }
 import java.math.{ BigDecimal, BigInteger }
 import java.util.LinkedHashMap
-import jawn.{ Facade, FContext, SupportParser }
+import jawn.{ CharBasedParser, Facade, FContext, SupportParser, SyncParser }
+import scala.util.Try
 
 final object CirceSupportParser extends SupportParser[Json] {
+  final override def parseUnsafe(s: String): Json = new StringParser(s).parse
+  final override def parseFromString(s: String): Try[Json] = Try(new StringParser(s).parse)
+
+  private[this] final class Slice(s: String, start: Int, limit: Int) extends CharSequence {
+    final val length: Int = limit - start
+    final def charAt(i: Int): Char = s.charAt(start + i)
+    final def subSequence(i: Int, j: Int): CharSequence = new Slice(s, start + i, start + j)
+    final override def toString: String = s.substring(start, limit)
+  }
+
+  private[this] final class StringParser(s: String) extends SyncParser[Json] with CharBasedParser[Json] {
+    var line: Int = 0
+    final def column(i: Int): Int = i
+    final def newline(i: Int): Unit = { line += 1 }
+    final def reset(i: Int): Int = i
+    final def checkpoint(state: Int, i: Int, stack: List[FContext[Json]]): Unit = ()
+    final def at(i: Int): Char = s.charAt(i)
+    final def at(i: Int, j: Int): CharSequence = new Slice(s, i, j)
+    final def atEof(i: Int): Boolean = i == s.length
+    final def close(): Unit = ()
+  }
+
   private[this] val jsonNumberParser: JsonNumberParser[Json] = new JsonNumberParser[Json] {
-    def createNegativeZeroValue: Json = Json.JNumber(JsonBiggerDecimal(BiggerDecimal.fromDoubleUnsafe(-0.0)))
-    def createUnsignedZeroValue: Json = Json.fromLong(0)
-    def createLongValue(value: Long): Json = Json.fromLong(value)
-    def createBigDecimalValue(unscaled: BigInteger, scale: Int): Json =
+    final def createNegativeZeroValue: Json = Json.JNumber(JsonBiggerDecimal(BiggerDecimal.fromDoubleUnsafe(-0.0)))
+    final def createUnsignedZeroValue: Json = Json.fromLong(0)
+    final def createLongValue(value: Long): Json = Json.fromLong(value)
+    final def createBigDecimalValue(unscaled: BigInteger, scale: Int): Json =
       Json.JNumber(JsonBigDecimal(new BigDecimal(unscaled, scale)))
-    def createBiggerDecimalValue(unscaled: BigInteger, scale: BigInteger): Json =
+    final def createBiggerDecimalValue(unscaled: BigInteger, scale: BigInteger): Json =
       Json.JNumber(JsonBiggerDecimal(BiggerDecimal(unscaled, scale)))
-    def failureValue: Json = null
+    final def failureValue: Json = null
   }
 
   implicit final val facade: Facade[Json] = new Facade[Json] {
@@ -42,7 +65,7 @@ final object CirceSupportParser extends SupportParser[Json] {
       final def isObj: Boolean = false
     }
 
-    def objectContext(): FContext[Json] = new FContext[Json] {
+    final def objectContext(): FContext[Json] = new FContext[Json] {
       private[this] final var key: String = null
       private[this] final val m = new LinkedHashMap[String, Json]
 
