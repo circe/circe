e6e6d266b9d49bfdc8127765f7aa1fd60c74c846
Travis Brown <travisbrown@stripe.com>
2017-07-09 17:40:21 -0500

    Make optics laws use our Shrink instances

--- modules/optics/src/test/scala/io/circe/optics/OpticsSuite.scala
+++ modules/optics/src/test/scala/io/circe/optics/OpticsSuite.scala
@@ -5,10 +5,7 @@ import io.circe.optics.all._
 import io.circe.tests.CirceSuite
 import io.circe.{ Json, JsonNumber, JsonObject }
 import monocle.function.Plated.plate
-import monocle.law.discipline.function.{ AtTests, EachTests, FilterIndexTests, IndexTests }
-import monocle.law.discipline.{ PrismTests, TraversalTests }
 import monocle.syntax.all._
-
 import scalaz.Equal
 import scalaz.std.anyVal._
 import scalaz.std.math.bigDecimal._
@@ -30,33 +27,33 @@ class OpticsSuite extends CirceSuite {
     (a.isNaN && b.isNaN) || scalaz.std.anyVal.doubleInstance.equal(a, b)
   }
 
-  checkLaws("Json to Unit", PrismTests(jsonNull))
-  checkLaws("Json to Boolean", PrismTests(jsonBoolean))
-  checkLaws("Json to BigDecimal", PrismTests(jsonBigDecimal))
-  checkLaws("Json to Double", PrismTests(jsonDouble))
-  checkLaws("Json to BigInt", PrismTests(jsonBigInt))
-  checkLaws("Json to Long", PrismTests(jsonLong))
-  checkLaws("Json to Int", PrismTests(jsonInt))
-  checkLaws("Json to Short", PrismTests(jsonShort))
-  checkLaws("Json to Byte", PrismTests(jsonByte))
-  checkLaws("Json to String", PrismTests(jsonString))
-  checkLaws("Json to JsonNumber", PrismTests(jsonNumber))
-  checkLaws("Json to JsonObject", PrismTests(jsonObject))
-  checkLaws("Json to Vector[Json]", PrismTests(jsonArray))
+  checkLaws("Json to Unit", LawsTests.prismTests(jsonNull))
+  checkLaws("Json to Boolean", LawsTests.prismTests(jsonBoolean))
+  checkLaws("Json to BigDecimal", LawsTests.prismTests(jsonBigDecimal))
+  checkLaws("Json to Double", LawsTests.prismTests(jsonDouble))
+  checkLaws("Json to BigInt", LawsTests.prismTests(jsonBigInt))
+  checkLaws("Json to Long", LawsTests.prismTests(jsonLong))
+  checkLaws("Json to Int", LawsTests.prismTests(jsonInt))
+  checkLaws("Json to Short", LawsTests.prismTests(jsonShort))
+  checkLaws("Json to Byte", LawsTests.prismTests(jsonByte))
+  checkLaws("Json to String", LawsTests.prismTests(jsonString))
+  checkLaws("Json to JsonNumber", LawsTests.prismTests(jsonNumber))
+  checkLaws("Json to JsonObject", LawsTests.prismTests(jsonObject))
+  checkLaws("Json to Vector[Json]", LawsTests.prismTests(jsonArray))
 
-  checkLaws("JsonNumber to BigDecimal", PrismTests(jsonNumberBigDecimal))
-  checkLaws("JsonNumber to BigInt", PrismTests(jsonNumberBigInt))
-  checkLaws("JsonNumber to Long", PrismTests(jsonNumberLong))
-  checkLaws("JsonNumber to Int", PrismTests(jsonNumberInt))
-  checkLaws("JsonNumber to Short", PrismTests(jsonNumberShort))
-  checkLaws("JsonNumber to Byte", PrismTests(jsonNumberByte))
+  checkLaws("JsonNumber to BigDecimal", LawsTests.prismTests(jsonNumberBigDecimal))
+  checkLaws("JsonNumber to BigInt", LawsTests.prismTests(jsonNumberBigInt))
+  checkLaws("JsonNumber to Long", LawsTests.prismTests(jsonNumberLong))
+  checkLaws("JsonNumber to Int", LawsTests.prismTests(jsonNumberInt))
+  checkLaws("JsonNumber to Short", LawsTests.prismTests(jsonNumberShort))
+  checkLaws("JsonNumber to Byte", LawsTests.prismTests(jsonNumberByte))
 
-  checkLaws("plated Json", TraversalTests(plate[Json]))
+  checkLaws("plated Json", LawsTests.traversalTests(plate[Json]))
 
-  checkLaws("jsonObjectEach", EachTests[JsonObject, Json])
-  checkLaws("jsonObjectAt", AtTests[JsonObject, String, Option[Json]])
-  checkLaws("jsonObjectIndex", IndexTests[JsonObject, String, Json])
-  checkLaws("jsonObjectFilterIndex", FilterIndexTests[JsonObject, String, Json])
+  checkLaws("jsonObjectEach", LawsTests.eachTests[JsonObject, Json])
+  checkLaws("jsonObjectAt", LawsTests.atTests[JsonObject, String, Option[Json]])
+  checkLaws("jsonObjectIndex", LawsTests.indexTests[JsonObject, String, Json])
+  checkLaws("jsonObjectFilterIndex", LawsTests.filterIndexTests[JsonObject, String, Json])
 
   "jsonDouble" should "round-trip in reverse with Double.NaN" in {
     assert(jsonDouble.getOption(jsonDouble.reverseGet(Double.NaN)) === Some(Double.NaN))
