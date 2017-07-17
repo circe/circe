e3ca40181629ee73f7a94e1f5f425fa2e5ab4e44
skolbasov <sergey.kolbasov@zalando.fi>
2017-07-18 00:04:46 +0300

    ScalaDoc for public API in circe-streaming

--- modules/streaming/src/main/scala/io/circe/streaming/package.scala
+++ modules/streaming/src/main/scala/io/circe/streaming/package.scala
@@ -6,18 +6,34 @@ import io.circe.jawn.CirceSupportParser
 import io.iteratee.{ Enumeratee, Enumerator }
 
 package object streaming {
+
+  /**
+    * String parser of JSON values wrapped in a single array
+    */
   final def stringArrayParser[F[_]](implicit F: ApplicativeError[F, Throwable]): Enumeratee[F, String, Json] =
     stringParser(AsyncParser.UnwrapArray)
 
+  /**
+    * String parser of JSON values delimited by whitespace/newline
+    */
   final def stringStreamParser[F[_]](implicit F: ApplicativeError[F, Throwable]): Enumeratee[F, String, Json] =
     stringParser(AsyncParser.ValueStream)
 
+  /**
+    * Byte array parser of JSON values wrapped in a single array
+    */
   final def byteArrayParser[F[_]](implicit F: ApplicativeError[F, Throwable]): Enumeratee[F, Array[Byte], Json] =
     byteParser(AsyncParser.UnwrapArray)
 
+  /**
+    * Byte array parser of JSON values delimited by whitespace/newline
+    */
   final def byteStreamParser[F[_]](implicit F: ApplicativeError[F, Throwable]): Enumeratee[F, Array[Byte], Json] =
     byteParser(AsyncParser.ValueStream)
 
+  /**
+    * Enumeratee decoder from JSON to some type `A`
+    */
   final def decoder[F[_], A](implicit F: MonadError[F, Throwable], decode: Decoder[A]): Enumeratee[F, Json, A] =
     Enumeratee.flatMap(json =>
       decode(json.hcursor) match {
