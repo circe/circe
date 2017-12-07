293e8493938c508692ca898710cdc6b87f3c9874
Travis Brown <travisrobertbrown@gmail.com>
2017-12-07 17:10:05 -0600

    Rearrange ConfiguredDecoder implementation to fix Serializable issue

--- modules/generic-extras/src/main/scala/io/circe/generic/extras/decoding/ConfiguredDecoder.scala
+++ modules/generic-extras/src/main/scala/io/circe/generic/extras/decoding/ConfiguredDecoder.scala
@@ -12,33 +12,22 @@ import shapeless.ops.record.Keys
 abstract class ConfiguredDecoder[A] extends DerivedDecoder[A]
 
 final object ConfiguredDecoder extends IncompleteConfiguredDecoders {
-  implicit def decodeCaseClass[A, R <: HList, D <: HList, F <: HList, K <: HList](implicit
+  private[this] class CaseClassConfiguredDecoder[A, R <: HList](
     gen: LabelledGeneric.Aux[A, R],
-    decode: Lazy[ReprDecoder[R]],
-    defaults: Default.AsRecord.Aux[A, D],
-    defaultMapper: RecordToMap[D],
+    decodeR: ReprDecoder[R],
     config: Configuration,
-    fields: Keys.Aux[R, F],
-    fieldsToList: ToTraversable.Aux[F, List, Symbol],
-    keys: Annotations.Aux[JsonKey, A, K],
-    keysToList: ToTraversable.Aux[K, List, Option[JsonKey]]
-  ): ConfiguredDecoder[A] = new ConfiguredDecoder[A] {
-    private[this] val defaultMap: Map[String, Any] =
-      if (config.useDefaults) defaultMapper(defaults()) else Map.empty
-
-    private[this] val keyAnnotations: List[Option[JsonKey]] = keysToList(keys())
-    private[this] val hasKeyAnnotations: Boolean = keyAnnotations.exists(_.nonEmpty)
-
-    private[this] val keyAnnotationMap: Map[String, String] =
-      fieldsToList(fields()).map(_.name).zip(keyAnnotations).collect {
-        case (field, Some(keyAnnotation)) => (field, keyAnnotation.value)
-      }.toMap
-
+    defaultMap: Map[String, Any],
+    keyAnnotationMap: Map[String, String]
+  ) extends ConfiguredDecoder[A] {
     private[this] def memberNameTransformer(transformMemberNames: String => String)(value: String): String =
       keyAnnotationMap.getOrElse(value, transformMemberNames(value))
 
-    final def apply(c: HCursor): Decoder.Result[A] = decode.value.configuredDecode(c)(
-      if (hasKeyAnnotations) memberNameTransformer(config.transformMemberNames) else config.transformMemberNames,
+    final def apply(c: HCursor): Decoder.Result[A] = decodeR.configuredDecode(c)(
+      if (keyAnnotationMap.nonEmpty) {
+        memberNameTransformer(config.transformMemberNames)
+      } else {
+        config.transformMemberNames
+      },
       config.transformConstructorNames,
       defaultMap,
       None
@@ -48,12 +37,38 @@ final object ConfiguredDecoder extends IncompleteConfiguredDecoders {
     }
 
     override def decodeAccumulating(c: HCursor): AccumulatingDecoder.Result[A] =
-      decode.value.configuredDecodeAccumulating(c)(
-        if (hasKeyAnnotations) memberNameTransformer(config.transformMemberNames) else config.transformMemberNames,
+      decodeR.configuredDecodeAccumulating(c)(
+        if (keyAnnotationMap.nonEmpty) {
+          memberNameTransformer(config.transformMemberNames)
+        } else {
+          config.transformMemberNames
+        },
         config.transformConstructorNames,
         defaultMap,
         None
       ).map(gen.from)
+  }
+
+  implicit def decodeCaseClass[A, R <: HList, D <: HList, F <: HList, K <: HList](implicit
+    gen: LabelledGeneric.Aux[A, R],
+    decodeR: Lazy[ReprDecoder[R]],
+    defaults: Default.AsRecord.Aux[A, D],
+    defaultMapper: RecordToMap[D],
+    config: Configuration,
+    fields: Keys.Aux[R, F],
+    fieldsToList: ToTraversable.Aux[F, List, Symbol],
+    keys: Annotations.Aux[JsonKey, A, K],
+    keysToList: ToTraversable.Aux[K, List, Option[JsonKey]]
+  ): ConfiguredDecoder[A] = {
+    val defaultMap: Map[String, Any] =
+      if (config.useDefaults) defaultMapper(defaults()) else Map.empty
+
+    val keyAnnotationMap: Map[String, String] =
+      fieldsToList(fields()).map(_.name).zip(keysToList(keys())).collect {
+        case (field, Some(keyAnnotation)) => (field, keyAnnotation.value)
+      }.toMap
+
+    new CaseClassConfiguredDecoder[A, R](gen, decodeR.value, config, defaultMap, keyAnnotationMap)
   }
 
   implicit def decodeAdt[A, R <: Coproduct](implicit
