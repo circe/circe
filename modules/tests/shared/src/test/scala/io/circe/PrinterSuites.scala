4f90743288a50e3d20b52edab654e006439d8f85
Harald Meland <hmeland@basefarm.no>
2017-12-07 13:12:09 +0100

    Printer support for escaping non-ASCII chars

--- modules/tests/shared/src/test/scala/io/circe/PrinterSuites.scala
+++ modules/tests/shared/src/test/scala/io/circe/PrinterSuites.scala
@@ -5,6 +5,14 @@ import io.circe.tests.PrinterSuite
 class Spaces2PrinterSuite extends PrinterSuite(Printer.spaces2, parser.`package`) with Spaces2PrinterExample
 class Spaces4PrinterSuite extends PrinterSuite(Printer.spaces4, parser.`package`)
 class NoSpacesPrinterSuite extends PrinterSuite(Printer.noSpaces, parser.`package`)
+class UnicodeEscapePrinterSuite extends PrinterSuite(Printer.noSpaces.escapeUnicode, parser.`package`) {
+  import io.circe.syntax._
+  "Printing object" should "unicode-escape all non-ASCII chars" in {
+    val actual = Json.obj("0 ℃" := "32 ℉").pretty(printer)
+    val expected = "{\"0 \\u2103\":\"32 \\u2109\"}"
+    assert(actual === expected)
+  }
+}
 
 class Spaces2PrinterWithWriterReuseSuite extends PrinterSuite(
   Printer.spaces2.copy(reuseWriters = true),
@@ -18,5 +26,10 @@ class Spaces4PrinterWithWriterReuseSuite extends PrinterSuite(
 
 class NoSpacesPrinterWithWriterReuseSuite extends PrinterSuite(
   Printer.noSpaces.copy(reuseWriters = true),
+  parser.`package`
+)
+
+class UnicodeEscapePrinterWithWriterReuseSuite extends PrinterSuite(
+  Printer.noSpaces.escapeUnicode.copy(reuseWriters = true),
   parser.`package`
 )
