d117781304c18ef5bb71de7f9fad6ce3cc9fe725
Travis Brown <travisrobertbrown@gmail.com>
2018-04-24 09:47:51 -0500

    Don't create a Kleisli instance for every JsonObject

--- modules/core/shared/src/main/scala/io/circe/JsonObject.scala
+++ modules/core/shared/src/main/scala/io/circe/JsonObject.scala
@@ -71,8 +71,7 @@ sealed abstract class JsonObject extends Serializable {
    *
    * @group Contents
    */
-  @transient
-  final val kleisli: Kleisli[Option, String, Json] = Kleisli(apply(_))
+  final def kleisli: Kleisli[Option, String, Json] = Kleisli(apply(_))
 
   /**
    * Return all keys in insertion order.
