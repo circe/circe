d0abab285d72e05de925a086d88526fe830c9da6
Travis Brown <travisbrown@stripe.com>
2018-08-19 19:32:03 +0200

    Add Factory instances for 2.13

--- modules/tests/shared/src/main/scala-2.13+/io/circe/CanBuildFromInstances.scala
+++ modules/tests/shared/src/main/scala-2.13+/io/circe/CanBuildFromInstances.scala
@@ -1,3 +1,25 @@
 package io.circe
 
-trait CanBuildFromInstances
+import scala.collection.{ Factory, IterableOnce }
+import scala.collection.mutable.{ ArrayBuffer, ArrayBuilder, Builder }
+import scala.reflect.ClassTag
+
+trait CanBuildFromInstances {
+  /**
+   * We need serializable `Factory` instances for arrays for our `Array` codec tests.
+   */
+  implicit def factoryRefArraySerializable[A <: AnyRef: ClassTag]: Factory[A, Array[A]] =
+    new Factory[A, Array[A]] with Serializable {
+      def fromSpecific(it: IterableOnce[A]): Array[A] = ArrayBuffer.from(it).toArray
+      def newBuilder: Builder[A, Array[A]] = new ArrayBuilder.ofRef[A]
+    }
+
+  /**
+   * We need serializable `Factory` instances for streams for our `NonEmptyStream` codec tests.
+   */
+  implicit def factoryStreamSerializable[A]: Factory[A, Stream[A]] =
+    new Factory[A, Stream[A]] with Serializable {
+      def fromSpecific(it: IterableOnce[A]): Stream[A] = Stream.from(it)
+      def newBuilder: Builder[A, Stream[A]] = Stream.newBuilder[A]
+    }
+}
