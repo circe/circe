dff37300ebb2b82b7116f674a0093b5c8836ac23
Travis Brown <travisbrown@stripe.com>
2018-08-20 12:00:15 +0200

    Avoid === in circe-testing

--- modules/testing/shared/src/main/scala/io/circe/testing/EqInstances.scala
+++ modules/testing/shared/src/main/scala/io/circe/testing/EqInstances.scala
@@ -2,8 +2,8 @@ package io.circe.testing
 
 import cats.instances.either._
 import cats.instances.option._
+import cats.instances.string._
 import cats.kernel.Eq
-import cats.syntax.eq._
 import io.circe.{ AccumulatingDecoder, ArrayEncoder, Decoder, Encoder, Json, KeyDecoder, KeyEncoder, ObjectEncoder }
 import org.scalacheck.Arbitrary
 
@@ -19,30 +19,34 @@ trait EqInstances { this: ArbitraryInstances =>
   ).flatten
 
   implicit def eqKeyEncoder[A: Arbitrary]: Eq[KeyEncoder[A]] = Eq.instance { (e1, e2) =>
-    arbitraryValues[A].take(codecEqualityCheckCount).forall(a => e1(a) == e2(a))
+    arbitraryValues[A].take(codecEqualityCheckCount).forall(a => Eq[String].eqv(e1(a), e2(a)))
   }
 
   implicit def eqKeyDecoder[A: Eq]: Eq[KeyDecoder[A]] = Eq.instance { (d1, d2) =>
-    arbitraryValues[String].take(codecEqualityCheckCount).forall(s => d1(s) === d2(s))
+    arbitraryValues[String].take(codecEqualityCheckCount).forall(s => Eq[Option[A]].eqv(d1(s), d2(s)))
   }
 
   implicit def eqEncoder[A: Arbitrary]: Eq[Encoder[A]] = Eq.instance { (e1, e2) =>
-    arbitraryValues[A].take(codecEqualityCheckCount).forall(a => e1(a) === e2(a))
+    arbitraryValues[A].take(codecEqualityCheckCount).forall(a => Eq[Json].eqv(e1(a), e2(a)))
   }
 
   implicit def eqDecoder[A: Eq]: Eq[Decoder[A]] = Eq.instance { (d1, d2) =>
-    arbitraryValues[Json].take(codecEqualityCheckCount).forall(json => d1(json.hcursor) === d2(json.hcursor))
+    arbitraryValues[Json].take(codecEqualityCheckCount).forall(json =>
+      Eq[Decoder.Result[A]].eqv(d1(json.hcursor), d2(json.hcursor))
+    )
   }
 
   implicit def eqObjectEncoder[A: Arbitrary]: Eq[ObjectEncoder[A]] = Eq.instance { (e1, e2) =>
-    arbitraryValues[A].take(codecEqualityCheckCount).forall(a => e1(a) === e2(a))
+    arbitraryValues[A].take(codecEqualityCheckCount).forall(a => Eq[Json].eqv(e1(a), e2(a)))
   }
 
   implicit def eqArrayEncoder[A: Arbitrary]: Eq[ArrayEncoder[A]] = Eq.instance { (e1, e2) =>
-    arbitraryValues[A].take(codecEqualityCheckCount).forall(a => e1(a) === e2(a))
+    arbitraryValues[A].take(codecEqualityCheckCount).forall(a => Eq[Json].eqv(e1(a), e2(a)))
   }
 
   implicit def eqAccumulatingDecoder[A: Eq]: Eq[AccumulatingDecoder[A]] = Eq.instance { (d1, d2) =>
-    arbitraryValues[Json].take(codecEqualityCheckCount).forall(json => d1(json.hcursor) === d2(json.hcursor))
+    arbitraryValues[Json].take(codecEqualityCheckCount).forall(json =>
+      Eq[AccumulatingDecoder.Result[A]].eqv(d1(json.hcursor), d2(json.hcursor))
+    )
   }
 }
