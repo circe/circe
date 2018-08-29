27d8fbf111d8a714de05498b21abfa8018d83f8d
Travis Brown <travisbrown@stripe.com>
2018-08-29 14:43:26 +0200

    Add failing test for SortedMap encoding

--- modules/tests/shared/src/test/scala/io/circe/EncoderSuite.scala
+++ modules/tests/shared/src/test/scala/io/circe/EncoderSuite.scala
@@ -3,6 +3,8 @@ package io.circe
 import cats.laws.discipline.ContravariantTests
 import io.circe.syntax._
 import io.circe.tests.CirceSuite
+import org.scalacheck.Arbitrary
+import scala.collection.SortedMap
 
 class EncoderSuite extends CirceSuite {
   checkLaws("Encoder[Int]", ContravariantTests[Encoder].contravariant[Int, Int, Int])
@@ -34,6 +36,25 @@ class EncoderSuite extends CirceSuite {
   "encodeList" should "match sequence encoders" in forAll { (xs: List[Int]) =>
     assert(Encoder.encodeList[Int].apply(xs) === Encoder[Seq[Int]].apply(xs))
   }
+
+  case class MyString(value: String)
+
+  object MyString {
+    implicit val myStringOrdering: Ordering[MyString] = Ordering.by[MyString, String](_.value).reverse
+    implicit val myStringKeyEncoder: KeyEncoder[MyString] = KeyEncoder.instance(_.value)
+    implicit val myStringArbitrary: Arbitrary[MyString] = Arbitrary(
+      Arbitrary.arbitrary[String].map(MyString(_))
+    )
+  }
+
+  "encodeMap" should "preserve insertion order" in forAll { (m: SortedMap[MyString, String]) =>
+    val Some(asJsonObject) = m.asJson.asObject
+    val expected = m.toList.map {
+      case (k, v) => MyString.myStringKeyEncoder(k) -> Json.fromString(v)
+    }
+
+    assert(asJsonObject.toList === expected)
+  } 
 
   "encodeVector" should "match sequence encoders" in forAll { (xs: Vector[Int]) =>
     assert(Encoder.encodeVector[Int].apply(xs) === Encoder[Seq[Int]].apply(xs))
