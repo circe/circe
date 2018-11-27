840cd8fe07c92cc84a6cc8e3036416919f48987f
Emanuele Pirro <epirro@powerspace.com>
2018-11-27 13:04:13 +0100

    Fix Scala formatter probems

--- modules/benchmark/src/main/scala/io/circe/benchmark/KeyCasingBenchmark.scala
+++ modules/benchmark/src/main/scala/io/circe/benchmark/KeyCasingBenchmark.scala
@@ -5,12 +5,12 @@ import java.util.concurrent.TimeUnit
 import org.openjdk.jmh.annotations._
 
 /**
-  * Measure the performance of the case transformations.
-  *
-  * The following command will run the benchmarks with reasonable settings:
-  *
-  * > `sbt "benchmark/jmh:run -i 10 -wi 10 -f 2 -t 1 io.circe.benchmark.KeyCasingBenchmark"`
-  */
+ * Measure the performance of the case transformations.
+ *
+ * The following command will run the benchmarks with reasonable settings:
+ *
+ * > `sbt "benchmark/jmh:run -i 10 -wi 10 -f 2 -t 1 io.circe.benchmark.KeyCasingBenchmark"`
+ */
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.SECONDS)
