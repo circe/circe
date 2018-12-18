16fc811bf4c7921f116eaa25686e67a02a129238
Travis Brown <travisrobertbrown@gmail.com>
2018-12-18 11:33:33 -0600

    Remove AnyVal constraint on *codeUnwrapped

--- modules/generic-extras/src/main/scala/io/circe/generic/extras/encoding/UnwrappedEncoder.scala
+++ modules/generic-extras/src/main/scala/io/circe/generic/extras/encoding/UnwrappedEncoder.scala
@@ -6,7 +6,7 @@ import shapeless.{ ::, Generic, HNil, Lazy }
 abstract class UnwrappedEncoder[A] extends Encoder[A]
 
 final object UnwrappedEncoder {
-  implicit def encodeUnwrapped[A <: AnyVal, R](
+  implicit def encodeUnwrapped[A, R](
     implicit
     gen: Lazy[Generic.Aux[A, R :: HNil]],
     encode: Encoder[R]
