6d50886db27f50b4e1c9392e377ef6d273952e10
Quentin ADAM <quentin.adam@waxzce.org>
2019-04-09 18:23:17 +0200

    first workink version, need a lot of cleanup and tests

---
+++ modules/squants/shared/src/main/scala/io/circe/squants/package.scala
@@ -0,0 +1,69 @@
+package io.circe
+
+import _root_.squants._
+
+import _root_.squants.Quantity
+
+import _root_.squants.experimental.formatter.implicits._
+import _root_.squants.experimental.formatter.DefaultFormatter
+import _root_.squants.experimental.unitgroups.information._
+import _root_.squants.experimental.formatter.syntax._
+import _root_.squants.information.InformationConversions._
+import _root_.squants.information.{Information, Megabytes}
+import _root_.squants.time.Frequency
+
+
+import scala.reflect.runtime.universe._
+
+
+/**
+ * Provides codecs for [[https://github.com/typelevel/squants squants]] types.
+ *
+ *
+ * @author Quentin ADAM @waxzce
+ */
+
+
+
+package object squants {
+
+
+
+  implicit def encodeSquantsQuantity[A <: Quantity[A]]: Encoder[A] = new Encoder[A] {
+    //    implicit val informationFormatter = new DefaultFormatter(MetricInformation) // TODO, be parametrizable
+
+    final def apply(a: A): Json = Json.obj(
+      //    ("readable", Json.fromString(a.inBestUnit.toString())),
+      ("number", Json.fromDoubleOrString(a.value)),
+      ("unit", Json.fromString(a.unit.symbol))
+      //   ("inPrimaryUnit_" + Information.primaryUnit.symbol, Json.fromDoubleOrString(a.to(Information.primaryUnit).doubleValue())))
+    ) //}
+  }
+
+
+  implicit def decodeSquantsQuantity[A <: Quantity[A]](implicit man: TypeTag[A]): Decoder[A] = new Decoder[A] {
+    final def apply(c: HCursor): Decoder.Result[A] =
+      for {
+    //    readable <- c.downField("readable").as[String]
+        number <- c.downField("number").as[Double]
+        unit <- c.downField("unit").as[String]
+      } yield {
+
+        val universeMirror = runtimeMirror(getClass.getClassLoader)
+        val companionMirror = universeMirror.reflectModule(typeOf[A].typeSymbol.companion.asModule)
+        val i = companionMirror.instance.asInstanceOf[Dimension[A]]
+
+        val d = i.primaryUnit.apply(number)
+        d
+
+
+      }
+  }
+
+  implicit def encodeInformation: Encoder[Information] = encodeSquantsQuantity[Information]
+  implicit def decodeInformation: Decoder[Information] = decodeSquantsQuantity[Information]
+
+
+
+
+}
---
+++ modules/squants/shared/src/test/scala/io/circe/squants/SquantsSuite.scala
@@ -0,0 +1,44 @@
+package io.circe.squants
+
+import cats.kernel.Eq
+
+import squants.information.Information
+
+
+
+import io.circe._
+import io.circe.syntax._
+import io.circe.parser.parse
+import io.circe.{Decoder, Encoder, Json, KeyDecoder, KeyEncoder}
+import io.circe.testing.CodecTests
+import io.circe.tests.CirceSuite
+import io.circe.squants._
+import org.scalacheck.{Arbitrary, Gen}
+import shapeless.{Nat, Witness => W}
+
+
+/**
+  * Provides codecs tests for [[https://github.com/typelevel/squants squants]] types.
+  *
+  *
+  * @author Quentin ADAM @waxzce
+  */
+
+class SquantsSuite extends CirceSuite {
+
+  "an Information should " should "encode and decode" in {
+    val n = Information.primaryUnit.apply(1)
+
+    val j = n.asJson
+    println(j)
+
+    val o = parse(j.toString).map(_.as[Information])
+
+    println(o)
+
+    println("----")
+    assert(o.toOption.isDefined)
+  }
+}
+
+
