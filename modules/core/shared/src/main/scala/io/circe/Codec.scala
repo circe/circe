76754bda6a42e51e6523901f11b667099374184e
Travis Brown <travisrobertbrown@gmail.com>
2019-05-30 07:37:46 -0500

    Minor changes to new Codec API and docs

--- modules/core/shared/src/main/scala/io/circe/Codec.scala
+++ modules/core/shared/src/main/scala/io/circe/Codec.scala
@@ -1,17 +1,52 @@
-import io.circe._
+package io.circe
 
 /**
  * A type class that provides back and forth conversion between values of type `A`
- * and the [[Json]] format. Must obey the laws defined in [[io.circe.tests.CodecLaws]].
+ * and the [[Json]] format.
+ *
+ * Note that this type class is only intended to make instance definition more
+ * convenient; it generally should not be used as a constraint.
+ *
+ * Instances should obey the laws defined in [[io.circe.testing.CodecLaws]].
  */
-trait Codec[A] extends Encoder[A] with Decoder[A]
+trait Codec[A] extends Decoder[A] with Encoder[A]
 
-object Codec {
+final object Codec {
   def apply[A](implicit instance: Codec[A]): Codec[A] = instance
 
-  implicit def fromEncoderDecoder[A](implicit e: Encoder[A], d: Decoder[A]): Codec[A] =
+  def from[A](decodeA: Decoder[A], encodeA: Encoder[A]): Codec[A] =
     new Codec[A] {
-      def apply(c: HCursor): Decoder.Result[A] = d(c)
-      def apply(a: A): Json = e(a)
+      def apply(c: HCursor): Decoder.Result[A] = decodeA(c)
+      def apply(a: A): Json = encodeA(a)
     }
+
+  trait AsRoot[A] extends Codec[A] with Encoder.AsRoot[A]
+
+  final object AsRoot {
+    def apply[A](implicit instance: AsRoot[A]): AsRoot[A] = instance
+  }
+
+  trait AsArray[A] extends AsRoot[A] with Encoder.AsArray[A]
+
+  final object AsArray {
+    def apply[A](implicit instance: AsArray[A]): AsArray[A] = instance
+
+    def from[A](decodeA: Decoder[A], encodeA: Encoder.AsArray[A]): AsArray[A] =
+      new AsArray[A] {
+        def apply(c: HCursor): Decoder.Result[A] = decodeA(c)
+        def encodeArray(a: A): Vector[Json] = encodeA.encodeArray(a)
+      }
+  }
+
+  trait AsObject[A] extends AsRoot[A] with Encoder.AsObject[A]
+
+  final object AsObject {
+    def apply[A](implicit instance: AsObject[A]): AsObject[A] = instance
+
+    def from[A](decodeA: Decoder[A], encodeA: Encoder.AsObject[A]): AsObject[A] =
+      new AsObject[A] {
+        def apply(c: HCursor): Decoder.Result[A] = decodeA(c)
+        def encodeObject(a: A): JsonObject = encodeA.encodeObject(a)
+      }
+  }
 }
