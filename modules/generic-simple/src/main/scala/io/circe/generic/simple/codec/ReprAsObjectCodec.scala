da9ffb254afed97377cb75b8e3d7891f3823d320
Travis Brown <travisrobertbrown@gmail.com>
2019-06-23 10:33:30 -0500

    Remove derivation macros from generic-simple

--- modules/generic-simple/src/main/scala/io/circe/generic/simple/codec/ReprAsObjectCodec.scala
+++ modules/generic-simple/src/main/scala/io/circe/generic/simple/codec/ReprAsObjectCodec.scala
@@ -1,9 +1,10 @@
 package io.circe.generic.simple.codec
 
-import io.circe.{ Codec, Decoder, HCursor, JsonObject }
-import io.circe.generic.simple.Deriver
-import scala.language.experimental.macros
-import shapeless.HNil
+import cats.Apply
+import cats.data.Validated
+import io.circe.{ Codec, Decoder, DecodingFailure, Encoder, HCursor, JsonObject }
+import shapeless.{ :+:, ::, CNil, Coproduct, HList, HNil, Inl, Inr, Lazy, Witness }
+import shapeless.labelled.{ FieldType, field }
 
 /**
  * A codec for a generic representation of a case class or ADT.
@@ -12,11 +13,128 @@ import shapeless.HNil
  */
 abstract class ReprAsObjectCodec[A] extends Codec.AsObject[A]
 
-final object ReprAsObjectCodec {
-  implicit def deriveReprAsObjectCodec[R]: ReprAsObjectCodec[R] = macro Deriver.deriveCodec[R]
-
-  val hnilReprCodec: ReprAsObjectCodec[HNil] = new ReprAsObjectCodec[HNil] {
+final object ReprAsObjectCodec extends LowPriorityReprCodecInstances {
+  implicit val codecForHNil: ReprAsObjectCodec[HNil] = new ReprAsObjectCodec[HNil] {
     def apply(c: HCursor): Decoder.Result[HNil] = Right(HNil)
     def encodeObject(a: HNil): JsonObject = JsonObject.empty
+  }
+
+  implicit def codecForHCons[K <: Symbol, H, T <: HList](
+    implicit
+    key: Witness.Aux[K],
+    decodeH: Decoder[H],
+    encodeH: Encoder[H],
+    codecForT: ReprAsObjectCodec[T]
+  ): ReprAsObjectCodec[FieldType[K, H] :: T] = new ReprAsObjectCodec[FieldType[K, H] :: T] {
+    def apply(c: HCursor): Decoder.Result[FieldType[K, H] :: T] = for {
+      h <- c.get(key.value.name)(decodeH)
+      t <- codecForT(c)
+    } yield field[K](h) :: t
+
+    override def decodeAccumulating(c: HCursor): Decoder.AccumulatingResult[FieldType[K, H] :: T] =
+      consResults[Decoder.AccumulatingResult, K, H, T](
+        decodeH.tryDecodeAccumulating(c.downField(key.value.name)),
+        codecForT.decodeAccumulating(c)
+      )
+
+    def encodeObject(a: FieldType[K, H] :: T): JsonObject = a match {
+      case h :: t => ((key.value.name, encodeH(h))) +: codecForT.encodeObject(t)
+    }
+  }
+
+  implicit val codecForCNil: ReprAsObjectCodec[CNil] = new ReprAsObjectCodec[CNil] {
+    def apply(c: HCursor): Decoder.Result[CNil] = Left(DecodingFailure("CNil", c.history))
+    def encodeObject(a: CNil): JsonObject =
+      sys.error("No JSON representation of CNil (this shouldn't happen)")
+  }
+
+  implicit def codecForCoproduct[K <: Symbol, L, R <: Coproduct](
+    implicit
+    key: Witness.Aux[K],
+    decodeL: Decoder[L],
+    encodeL: Encoder[L],
+    codecForR: ReprAsObjectCodec[R]
+  ): ReprAsObjectCodec[FieldType[K, L] :+: R] = new ReprAsObjectCodec[FieldType[K, L] :+: R] {
+    def apply(c: HCursor): Decoder.Result[FieldType[K, L] :+: R] =
+      c.downField(key.value.name).focus match {
+        case Some(value) => value.as(decodeL).map(l => Inl(field(l)))
+        case None        => codecForR(c).map(Inr(_))
+      }
+
+    override def decodeAccumulating(c: HCursor): Decoder.AccumulatingResult[FieldType[K, L] :+: R] = {
+      val f = c.downField(key.value.name)
+
+      f.focus match {
+        case Some(value) => decodeL.tryDecodeAccumulating(f).map(l => Inl(field(l)))
+        case None        => codecForR.decodeAccumulating(c).map(Inr(_))
+      }
+    }
+
+    def encodeObject(a: FieldType[K, L] :+: R): JsonObject = a match {
+      case Inl(l) => JsonObject.singleton(key.value.name, encodeL(l))
+      case Inr(r) => codecForR.encodeObject(r)
+    }
+  }
+
+}
+
+trait LowPriorityReprCodecInstances {
+  def consResults[F[_], K, V, T <: HList](hv: F[V], tr: F[T])(
+    implicit F: Apply[F]
+  ): F[FieldType[K, V] :: T] =
+    F.map2(hv, tr)((v, t) => field[K].apply[V](v) :: t)
+
+  def injectLeftValue[K, V, R <: Coproduct](v: V): FieldType[K, V] :+: R = Inl(field[K].apply[V](v))
+
+  val hnilResult: Decoder.Result[HNil] = Right(HNil)
+  val hnilResultAccumulating: Decoder.AccumulatingResult[HNil] = Validated.valid(HNil)
+
+  implicit def codecForHConsDerived[K <: Symbol, H, T <: HList](
+    implicit
+    key: Witness.Aux[K],
+    codecForH: Lazy[DerivedAsObjectCodec[H]],
+    codecForT: Lazy[ReprAsObjectCodec[T]]
+  ): ReprAsObjectCodec[FieldType[K, H] :: T] = new ReprAsObjectCodec[FieldType[K, H] :: T] {
+    def apply(c: HCursor): Decoder.Result[FieldType[K, H] :: T] = for {
+      h <- c.get(key.value.name)(codecForH.value)
+      t <- codecForT.value(c)
+    } yield field[K](h) :: t
+
+    override def decodeAccumulating(c: HCursor): Decoder.AccumulatingResult[FieldType[K, H] :: T] =
+      consResults[Decoder.AccumulatingResult, K, H, T](
+        codecForH.value.tryDecodeAccumulating(c.downField(key.value.name)),
+        codecForT.value.decodeAccumulating(c)
+      )
+
+    def encodeObject(a: FieldType[K, H] :: T): JsonObject = a match {
+      case h :: t => ((key.value.name, codecForH.value(h))) +: codecForT.value.encodeObject(t)
+    }
+  }
+
+  implicit def codecForCoproductDerived[K <: Symbol, L, R <: Coproduct](
+    implicit
+    key: Witness.Aux[K],
+    codecForL: Lazy[DerivedAsObjectCodec[L]],
+    codecForR: Lazy[ReprAsObjectCodec[R]]
+  ): ReprAsObjectCodec[FieldType[K, L] :+: R] = new ReprAsObjectCodec[FieldType[K, L] :+: R] {
+    def apply(c: HCursor): Decoder.Result[FieldType[K, L] :+: R] =
+      c.downField(key.value.name).focus match {
+        case Some(value) => value.as(codecForL.value).map(l => Inl(field(l)))
+        case None        => codecForR.value(c).map(Inr(_))
+      }
+
+    override def decodeAccumulating(c: HCursor): Decoder.AccumulatingResult[FieldType[K, L] :+: R] = {
+      val f = c.downField(key.value.name)
+
+      f.focus match {
+        case Some(value) => codecForL.value.tryDecodeAccumulating(f).map(l => Inl(field(l)))
+        case None        => codecForR.value.decodeAccumulating(c).map(Inr(_))
+      }
+    }
+
+    def encodeObject(a: FieldType[K, L] :+: R): JsonObject = a match {
+      case Inl(l) => JsonObject.singleton(key.value.name, codecForL.value(l))
+      case Inr(r) => codecForR.value.encodeObject(r)
+    }
   }
 }
--- modules/generic-simple/src/main/scala/io/circe/generic/simple/encoding/ReprAsObjectEncoder.scala
+++ modules/generic-simple/src/main/scala/io/circe/generic/simple/encoding/ReprAsObjectEncoder.scala
@@ -1,8 +1,8 @@
 package io.circe.generic.simple.encoding
 
-import io.circe.Encoder
-import io.circe.generic.simple.Deriver
-import scala.language.experimental.macros
+import io.circe.{ Encoder, JsonObject }
+import shapeless.{ :+:, ::, CNil, Coproduct, HList, HNil, Inl, Inr, Lazy, Witness }
+import shapeless.labelled.FieldType
 
 /**
  * An encoder for a generic representation of a case class or ADT.
@@ -11,6 +11,61 @@ import scala.language.experimental.macros
  */
 abstract class ReprAsObjectEncoder[A] extends Encoder.AsObject[A]
 
-final object ReprAsObjectEncoder {
-  implicit def deriveReprAsObjectEncoder[R]: ReprAsObjectEncoder[R] = macro Deriver.deriveEncoder[R]
+final object ReprAsObjectEncoder extends LowPriorityReprAsObjectEncoderInstances {
+  implicit val encodeHNil: ReprAsObjectEncoder[HNil] = new ReprAsObjectEncoder[HNil] {
+    def encodeObject(a: HNil): JsonObject = JsonObject.empty
+  }
+
+  implicit def encodeHCons[K <: Symbol, H, T <: HList](
+    implicit
+    key: Witness.Aux[K],
+    encodeH: Lazy[Encoder[H]],
+    encodeT: Lazy[ReprAsObjectEncoder[T]]
+  ): ReprAsObjectEncoder[FieldType[K, H] :: T] = new ReprAsObjectEncoder[FieldType[K, H] :: T] {
+    def encodeObject(a: FieldType[K, H] :: T): JsonObject = a match {
+      case h :: t => ((key.value.name, encodeH.value(h))) +: encodeT.value.encodeObject(t)
+    }
+  }
+
+  implicit val encodeCNil: ReprAsObjectEncoder[CNil] = new ReprAsObjectEncoder[CNil] {
+    def encodeObject(a: CNil): JsonObject =
+      sys.error("No JSON representation of CNil (this shouldn't happen)")
+  }
+
+  implicit def encodeCoproduct[K <: Symbol, L, R <: Coproduct](
+    implicit
+    key: Witness.Aux[K],
+    encodeL: Lazy[Encoder[L]],
+    encodeR: Lazy[ReprAsObjectEncoder[R]]
+  ): ReprAsObjectEncoder[FieldType[K, L] :+: R] = new ReprAsObjectEncoder[FieldType[K, L] :+: R] {
+    def encodeObject(a: FieldType[K, L] :+: R): JsonObject = a match {
+      case Inl(l) => JsonObject.singleton(key.value.name, encodeL.value(l))
+      case Inr(r) => encodeR.value.encodeObject(r)
+    }
+  }
+}
+
+private[circe] trait LowPriorityReprAsObjectEncoderInstances {
+  implicit def encodeHConsDerived[K <: Symbol, H, T <: HList](
+    implicit
+    key: Witness.Aux[K],
+    encodeH: Lazy[DerivedAsObjectEncoder[H]],
+    encodeT: Lazy[ReprAsObjectEncoder[T]]
+  ): ReprAsObjectEncoder[FieldType[K, H] :: T] = new ReprAsObjectEncoder[FieldType[K, H] :: T] {
+    def encodeObject(a: FieldType[K, H] :: T): JsonObject = a match {
+      case h :: t => ((key.value.name, encodeH.value(h))) +: encodeT.value.encodeObject(t)
+    }
+  }
+
+  implicit def encodeCoproductDerived[K <: Symbol, L, R <: Coproduct](
+    implicit
+    key: Witness.Aux[K],
+    encodeL: Lazy[DerivedAsObjectEncoder[L]],
+    encodeR: Lazy[ReprAsObjectEncoder[R]]
+  ): ReprAsObjectEncoder[FieldType[K, L] :+: R] = new ReprAsObjectEncoder[FieldType[K, L] :+: R] {
+    def encodeObject(a: FieldType[K, L] :+: R): JsonObject = a match {
+      case Inl(l) => JsonObject.singleton(key.value.name, encodeL.value(l))
+      case Inr(r) => encodeR.value.encodeObject(r)
+    }
+  }
 }
