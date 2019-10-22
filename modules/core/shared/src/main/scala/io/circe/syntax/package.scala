3bb017bf416593da568c79726183c4aa9aaa6d3f
Travis Brown <travisrobertbrown@gmail.com>
2019-10-22 03:12:02 -0500

    Temporarily add deprecated methods for value class vals

--- modules/core/shared/src/main/scala/io/circe/syntax/package.scala
+++ modules/core/shared/src/main/scala/io/circe/syntax/package.scala
@@ -4,12 +4,16 @@ package io.circe
  * This package provides syntax via enrichment classes.
  */
 package object syntax {
-  implicit final class EncoderOps[A](private val wrappedEncodeable: A) extends AnyVal {
-    final def asJson(implicit encoder: Encoder[A]): Json = encoder(wrappedEncodeable)
+  implicit final class EncoderOps[A](private val value: A) extends AnyVal {
+    @deprecated("Do not use", "0.12.3")
+    def wrappedEncodeable: A = value
+    final def asJson(implicit encoder: Encoder[A]): Json = encoder(value)
     final def asJsonObject(implicit encoder: Encoder.AsObject[A]): JsonObject =
-      encoder.encodeObject(wrappedEncodeable)
+      encoder.encodeObject(value)
   }
-  implicit final class KeyOps[K](private val key: K) extends AnyVal {
-    final def :=[A: Encoder](a: A)(implicit keyEncoder: KeyEncoder[K]): (String, Json) = (keyEncoder(key), a.asJson)
+  implicit final class KeyOps[K](private val value: K) extends AnyVal {
+    @deprecated("Do not use", "0.12.3")
+    def key: K = value
+    final def :=[A: Encoder](a: A)(implicit keyEncoder: KeyEncoder[K]): (String, Json) = (keyEncoder(value), a.asJson)
   }
 }
