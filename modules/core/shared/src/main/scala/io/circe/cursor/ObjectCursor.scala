878d5824d056cf48b781851f41a4a70f59d8de6c
Travis Brown <travisrobertbrown@gmail.com>
2020-01-18 11:56:29 -0600

    Revert "Remove implementations of deprecated methods"
    This reverts commit e53dbcc2e2391e280d2edccdb7f02701bbe217b1.

--- modules/core/shared/src/main/scala/io/circe/cursor/ObjectCursor.scala
+++ modules/core/shared/src/main/scala/io/circe/cursor/ObjectCursor.scala
@@ -28,4 +28,5 @@ private[circe] final class ObjectCursor(obj: JsonObject, key: String, parent: HC
 
   def left: ACursor = fail(CursorOp.MoveLeft)
   def right: ACursor = fail(CursorOp.MoveRight)
+  def first: ACursor = fail(CursorOp.MoveFirst)
 }
