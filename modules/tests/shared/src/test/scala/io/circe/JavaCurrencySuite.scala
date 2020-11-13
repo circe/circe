278fa3f0d7a4b62530f2c93263159da217217947
David Strawn <isomarcte@gmail.com>
2020-11-13 13:23:14 -0700

    Add Instances ISO 4217 Currency Values
    This commit adds an Encoder and Decoder instance for ISO 4217 currency values, as modeled by the JRE as `java.util.Currency`.

---
+++ modules/tests/shared/src/test/scala/io/circe/JavaCurrencySuite.scala
@@ -0,0 +1,27 @@
+package io.circe
+
+import cats._
+import io.circe.testing.CodecTests
+import io.circe.tests.CirceSuite
+import java.util.Currency
+import org.scalacheck._
+import scala.collection.JavaConverters._
+
+final class JavaCurrencySuite extends CirceSuite {
+  import JavaCurrencySuite._
+
+  checkAll("Codec[Currency]", CodecTests[Currency].codec)
+}
+
+object JavaCurrencySuite {
+
+  lazy val availableCurrencies: Set[Currency] =
+    Currency.getAvailableCurrencies.asScala.toSet
+
+  implicit lazy val arbitraryCurrency: Arbitrary[Currency] =
+    Arbitrary(Gen.oneOf(availableCurrencies))
+
+  // Orphans
+  implicit private lazy val eqInstance: Eq[Currency] =
+    Eq.fromUniversalEquals
+}
