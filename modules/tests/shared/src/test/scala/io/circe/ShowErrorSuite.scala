a0bdaed495b7b371e121625bc011fcc03a818af9
Travis Brown <travisrobertbrown@gmail.com>
2020-11-28 06:19:33 -0600

    Move ShowErrorSuite from ScalaTest to MUnit

--- modules/tests/shared/src/test/scala/io/circe/ShowErrorSuite.scala
+++ modules/tests/shared/src/test/scala/io/circe/ShowErrorSuite.scala
@@ -5,7 +5,8 @@ import cats.syntax.eq._
 import cats.syntax.show._
 import io.circe.CursorOp._
 import io.circe.tests.CirceSuite
-import org.scalacheck.Gen
+import munit.ScalaCheckSuite
+import org.scalacheck.{ Gen, Prop }
 
 trait GenCursorOps {
   val arrayMoves: Gen[List[CursorOp]] = Gen.listOf(
@@ -20,18 +21,18 @@ trait GenCursorOps {
   val downFields: Gen[List[CursorOp]] = Gen.listOf(Gen.identifier.map(DownField))
 }
 
-class ShowErrorSuite extends CirceSuite with GenCursorOps {
-  "Show[ParsingFailure]" should "produce the expected output" in {
+class ShowErrorSuite extends ScalaCheckSuite with GenCursorOps {
+  test("Show[ParsingFailure] should produce the expected output") {
     assert(ParsingFailure("the message", new Exception()).show === "ParsingFailure: the message")
   }
 
-  "Show[DecodingFailure]" should "produce the expected output on a small example" in {
+  test("Show[DecodingFailure] should produce the expected output on a small example") {
     val ops = List(MoveRight, MoveRight, DownArray, DownField("bar"), DownField("foo"))
 
     assert(DecodingFailure("the message", ops).show === "DecodingFailure at .foo.bar[2]: the message")
   }
 
-  it should "produce the expected output on a larger example" in {
+  test("Show[DecodingFailure] should produce the expected output on a larger example") {
     val ops = List(
       MoveLeft,
       LeftN(2),
@@ -50,27 +51,31 @@ class ShowErrorSuite extends CirceSuite with GenCursorOps {
     assert(DecodingFailure("the message", ops).show === expected)
   }
 
-  it should "display field selection" in forAll(downFields) { moves =>
-    val selection = moves.foldRight("") {
-      case (DownField(f), s) => s"$s.$f"
-      case (_, s)            => s
-    }
+  test("Show[DecodingFailure] should display field selection") {
+    Prop.forAll(downFields) { moves =>
+      val selection = moves.foldRight("") {
+        case (DownField(f), s) => s"$s.$f"
+        case (_, s)            => s
+      }
 
-    val expected = s"DecodingFailure at $selection: the message"
-    assert(DecodingFailure("the message", moves).show === expected)
+      val expected = s"DecodingFailure at $selection: the message"
+      DecodingFailure("the message", moves).show === expected
+    }
   }
 
-  it should "display array indexing" in forAll(arrayMoves) { moves =>
-    val ops = moves :+ DownArray
-    val index = moves.foldLeft(0) {
-      case (i, MoveLeft)  => i - 1
-      case (i, MoveRight) => i + 1
-      case (i, LeftN(n))  => i - n
-      case (i, RightN(n)) => i + n
-      case (i, _)         => i
-    }
+  test("Show[DecodingFailure] should display array indexing") {
+    Prop.forAll(arrayMoves) { moves =>
+      val ops = moves :+ DownArray
+      val index = moves.foldLeft(0) {
+        case (i, MoveLeft)  => i - 1
+        case (i, MoveRight) => i + 1
+        case (i, LeftN(n))  => i - n
+        case (i, RightN(n)) => i + n
+        case (i, _)         => i
+      }
 
-    val expected = s"DecodingFailure at [$index]: the message"
-    assert(DecodingFailure("the message", ops).show === expected)
+      val expected = s"DecodingFailure at [$index]: the message"
+      DecodingFailure("the message", ops).show === expected
+    }
   }
 }
