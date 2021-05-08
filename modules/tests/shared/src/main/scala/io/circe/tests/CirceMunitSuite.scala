aca71b91728abb43607093e1b4140ee885a5d0a3
Diego E. Alonso Blas <diesalbla@gmail.com>
2021-05-08 01:23:41 +0100

    Migrate tests from ScalaTest to Munit: DecoderSuite
    Add duplicated traits for `LargeNumberDecoderTests`, temporary
    while we migrate the rest of existing tests until a next PR.

--- modules/tests/shared/src/main/scala/io/circe/tests/CirceMunitSuite.scala
+++ modules/tests/shared/src/main/scala/io/circe/tests/CirceMunitSuite.scala
@@ -6,4 +6,16 @@ import munit.DisciplineSuite
 /**
  * An opinionated stack of traits to improve consistency and reduce boilerplate in circe tests.
  */
-trait CirceMunitSuite extends DisciplineSuite with ArbitraryInstances with EqInstances with MissingInstances
+trait CirceMunitSuite extends DisciplineSuite with ArbitraryInstances with EqInstances with MissingInstances {
+
+  protected def group(name: String)(thunk: => Unit): Unit = {
+    val countBefore = munitTestsBuffer.size
+    val _ = thunk
+    val countAfter = munitTestsBuffer.size
+    val countRegistered = countAfter - countBefore
+    val registered = munitTestsBuffer.toList.drop(countBefore)
+    (0 until countRegistered).foreach(_ => munitTestsBuffer.remove(countBefore))
+    registered.foreach(t => munitTestsBuffer += t.withName(s"$name - ${t.name}"))
+  }
+
+}
