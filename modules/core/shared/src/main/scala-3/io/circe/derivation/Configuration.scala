b560861a63a92800c0f46f59b43bf465686ff83c
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2021-07-24 20:35:00 +0100

    Fix error in decodeSum.
    Add renaming object (copied from circe-derivation).
    More ergonomic Configuration class.

--- modules/core/shared/src/main/scala-3/io/circe/derivation/Configuration.scala
+++ modules/core/shared/src/main/scala-3/io/circe/derivation/Configuration.scala
@@ -1,7 +1,17 @@
 package io.circe.derivation
 
+object Configuration {
+  val default: Configuration = Configuration()
+}
 case class Configuration(
   transformNames: String => String = Predef.identity,
   useDefaults: Boolean = true,
   discriminator: Option[String] = None,
-)
+) {
+  def withTransformNames(f: String => String): Configuration = this.copy(transformNames = f)
+  def withSnakeCaseNames: Configuration = this.copy(transformNames = renaming.snakeCase)
+  def withKebabCaseNames: Configuration = this.copy(transformNames = renaming.kebabCase)
+  def withDefaults: Configuration = this.copy(useDefaults = true)
+  def withoutDefaults: Configuration = this.copy(useDefaults = false)
+  def withDiscriminator(discriminator: String): Configuration = this.copy(discriminator = Some(discriminator))
+}
