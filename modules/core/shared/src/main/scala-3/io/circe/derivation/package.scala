ec8b67bdca88ba56c40d5602233b5a0287633c48
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2021-07-26 03:46:58 +0100

    Add EnumEncoder, EnumDecoder, and EnumCodec

--- modules/core/shared/src/main/scala-3/io/circe/derivation/package.scala
+++ modules/core/shared/src/main/scala-3/io/circe/derivation/package.scala
@@ -1,6 +1,6 @@
 package io.circe.derivation
 
-import scala.compiletime.{constValue, erasedValue, summonFrom}
+import scala.compiletime.{constValue, erasedValue, error, summonFrom, summonInline}
 import scala.deriving.Mirror
 import io.circe.{Decoder, Encoder, Codec}
 
@@ -32,4 +32,14 @@ inline final def summonDecoder[A]: Decoder[A] =
   summonFrom {
     case decodeA: Decoder[A] => decodeA
     case _: Mirror.Of[A] => Decoder.derived[A]
+  }
+
+inline def summonSingletonCases[T <: Tuple, A](typeName: String): List[A] =
+  inline erasedValue[T] match {
+    case _: EmptyTuple => Nil
+    case _: (h *: t) =>
+      inline summonInline[Mirror.Of[h]] match {
+        case m: Mirror.Singleton => m.fromProduct(EmptyTuple).asInstanceOf[A] :: summonSingletonCases[t, A](typeName)
+        case m: Mirror => error("Enum " + typeName + " contains non singleton case " + constValue[m.MirroredLabel])
+      }
   }
