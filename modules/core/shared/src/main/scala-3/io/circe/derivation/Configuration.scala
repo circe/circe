11251ab1f46ba2d5439a9ca57261a773a08bc2b8
Simão Martins <simao.martins@tecnico.ulisboa.pt>
2021-08-20 18:07:38 +0100

    Add support for transform construtor and member names.
    Refactor ConfiguredEncoder implementation.

--- modules/core/shared/src/main/scala-3/io/circe/derivation/Configuration.scala
+++ modules/core/shared/src/main/scala-3/io/circe/derivation/Configuration.scala
@@ -3,19 +3,42 @@ package io.circe.derivation
 object Configuration:
   val default: Configuration = Configuration()
   val empty: Configuration = Configuration(useDefaults = false)
+
+/**
+ * Configuration allowing customisation of the JSON produced when encoding, or expected when decoding.
+ *
+ * @param transformMemberNames Transforms the names of any case class members in the JSON allowing, for example,
+ *                             formatting or case changes.
+ * @param useDefaults Whether to allow default values as specified for any case-class members.
+ * @param discriminator Optional key name that, when given, will be used to store the name of the constructor of an ADT
+ *                      in a nested field with this name. If not given, the name is instead stored as a key under which
+ *                      the contents of the ADT are stored as an object.
+ * @param transformConstructorNames Transforms the value of any constructor names in the JSON allowing, for example,
+ *                                  formatting or case changes.
+ * @param strictDecoding Whether to fail when superfluous fields are found.
+ */
 case class Configuration(
-  transformNames: String => String = Predef.identity,
+  transformMemberNames: String => String = Predef.identity,
+  transformConstructorNames: String => String = Predef.identity,
   useDefaults: Boolean = true,
   discriminator: Option[String] = None,
   strictDecoding: Boolean = false,
 ):
-  def withTransformNames(f: String => String): Configuration = this.copy(transformNames = f)
-  def withSnakeCaseNames: Configuration = this.copy(transformNames = renaming.snakeCase)
-  def withScreamingSnakeCaseNames: Configuration = this.copy(transformNames = renaming.screamingSnakeCase)
-  def withKebabCaseNames: Configuration = this.copy(transformNames = renaming.kebabCase)
-  def withDefaults: Configuration = this.copy(useDefaults = true)
-  def withoutDefaults: Configuration = this.copy(useDefaults = false)
-  def withDiscriminator(discriminator: String): Configuration = this.copy(discriminator = Some(discriminator))
-  def withoutDiscriminator: Configuration = this.copy(discriminator = None)
-  def withStrictDecoding: Configuration = this.copy(strictDecoding = true)
-  def withoutStrictDecoding: Configuration = this.copy(strictDecoding = false)
+  def withTransformMemberNames(f: String => String): Configuration = copy(transformMemberNames = f)
+  def withSnakeCaseMemberNames: Configuration = withTransformMemberNames(renaming.snakeCase)
+  def withScreamingSnakeCaseMemberNames: Configuration = withTransformMemberNames(renaming.screamingSnakeCase)
+  def withKebabCaseMemberNames: Configuration = withTransformMemberNames(renaming.kebabCase)
+  
+  def withTransformConstructorNames(f: String => String): Configuration = copy(transformConstructorNames = f)
+  def withSnakeCaseConstructorNames: Configuration = withTransformConstructorNames(renaming.snakeCase)
+  def withScreamingSnakeCaseConstructorNames: Configuration = withTransformConstructorNames(renaming.screamingSnakeCase)
+  def withKebabCaseConstructorNames: Configuration = withTransformConstructorNames(renaming.kebabCase)
+  
+  def withDefaults: Configuration = copy(useDefaults = true)
+  def withoutDefaults: Configuration = copy(useDefaults = false)
+  
+  def withDiscriminator(discriminator: String): Configuration = copy(discriminator = Some(discriminator))
+  def withoutDiscriminator: Configuration = copy(discriminator = None)
+  
+  def withStrictDecoding: Configuration = copy(strictDecoding = true)
+  def withoutStrictDecoding: Configuration = copy(strictDecoding = false)
