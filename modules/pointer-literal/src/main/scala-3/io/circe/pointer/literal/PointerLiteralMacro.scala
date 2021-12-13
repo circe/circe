956aca1ea1b4b73bc19f927a3f4489b4bf53eae7
Darren Gibson <darren.gibson@banno.com>
2021-12-13 08:53:47 -0600

    run scalafmt

--- modules/pointer-literal/src/main/scala-3/io/circe/pointer/literal/PointerLiteralMacro.scala
+++ modules/pointer-literal/src/main/scala-3/io/circe/pointer/literal/PointerLiteralMacro.scala
@@ -9,22 +9,25 @@ private object PointerLiteralMacros {
   final def pointerImpl(sc: Expr[StringContext], argsExpr: Expr[Seq[Any]])(using q: Quotes): Expr[Pointer] = {
     import q.reflect.*
     val stringParts: Seq[String] = sc match {
-      case '{StringContext($parts:_*)} => parts.valueOrAbort
+      case '{ StringContext($parts: _*) } => parts.valueOrAbort
     }
 
     if (Pointer.parse(stringParts.mkString("X")).isRight) {
-      val args = argsExpr match { 
-      case Varargs(argExprs) => argExprs
-      case other => report.error("Invalid arguments for pointer literal.");Nil
-    }
+      val args = argsExpr match {
+        case Varargs(argExprs) => argExprs
+        case other             => report.error("Invalid arguments for pointer literal."); Nil
+      }
       val input = args.zip(stringParts.tail).foldLeft(Expr(stringParts.head)) {
-        case (acc, (a, p)) => 
+        case (acc, (a, p)) =>
           val pExpr = Expr(p)
-          '{$acc + $a.toString.replaceAll("~", "~0").replaceAll("/", "~1") + $pExpr }
+          '{ $acc + $a.toString.replaceAll("~", "~0").replaceAll("/", "~1") + $pExpr }
       }
 
-      val t = '{_root_.io.circe.pointer.Pointer.parse($input)
-        .asInstanceOf[_root_.scala.Right[_root_.scala.Nothing, _root_.io.circe.pointer.Pointer]].value
+      val t = '{
+        _root_.io.circe.pointer.Pointer
+          .parse($input)
+          .asInstanceOf[_root_.scala.Right[_root_.scala.Nothing, _root_.io.circe.pointer.Pointer]]
+          .value
       }
 
       t
--- modules/pointer-literal/src/main/scala-3/io/circe/pointer/literal/package.scala
+++ modules/pointer-literal/src/main/scala-3/io/circe/pointer/literal/package.scala
@@ -3,7 +3,7 @@ package io.circe.pointer
 import scala.language.experimental.macros
 
 package object literal {
-   extension (inline sc:StringContext) {
-    inline final def pointer(inline args:Any*):Pointer = ${ PointerLiteralMacros.pointerImpl('sc,'args) }
+  extension (inline sc: StringContext) {
+    inline final def pointer(inline args: Any*): Pointer = ${ PointerLiteralMacros.pointerImpl('sc, 'args) }
   }
 }
