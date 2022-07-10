9c7ceb259c5f0a09daf07a5823d8a89f662e5b42
David Strawn <isomarcte@gmail.com>
2022-07-10 10:16:08 -0600

    Scalafmt

--- modules/pointer/src/test/scala/io/circe/pointer/ScalaCheckInstances.scala
+++ modules/pointer/src/test/scala/io/circe/pointer/ScalaCheckInstances.scala
@@ -4,11 +4,12 @@ import org.scalacheck._
 
 private[pointer] trait ScalaCheckInstances {
 
-  /** Generates a `String` which is a valid Object or Array reference according
-    * to RFC 6901 for JSON pointers.
-    *
-    * The primary purpose of this generator is to handle escapes correctly.
-    */
+  /**
+   * Generates a `String` which is a valid Object or Array reference according
+   * to RFC 6901 for JSON pointers.
+   *
+   * The primary purpose of this generator is to handle escapes correctly.
+   */
   final val genPointerReferenceString: Gen[String] =
     Arbitrary.arbitrary[String].map(_.replaceAll("~", "~0").replaceAll("/", "~1"))
 }
