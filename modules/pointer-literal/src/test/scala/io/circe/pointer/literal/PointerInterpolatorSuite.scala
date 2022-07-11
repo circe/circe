04212dfc11ccc66a44bf03d626f5d5374cb2c3ca
David Strawn <isomarcte@gmail.com>
2022-07-11 07:28:29 -0600

    Unify The Majority Of Pointer Literal Tests
    Only the tests which check the compile error string are specialized to specific Scala versions now. This is because the way that Scala 2/3 macros build up the string describing the position is slightly different.

--- modules/pointer-literal/src/test/scala/io/circe/pointer/literal/PointerInterpolatorSuite.scala
+++ modules/pointer-literal/src/test/scala/io/circe/pointer/literal/PointerInterpolatorSuite.scala
@@ -1,11 +1,10 @@
 package io.circe.pointer.literal
 
-import io.circe.pointer.Pointer
+import io.circe.pointer._
 import munit.ScalaCheckSuite
-import org.scalacheck.Prop
-import io.circe.pointer.ScalaCheckInstances
+import org.scalacheck._
 
-class PointerInterpolatorSuite extends ScalaCheckSuite {
+final class PointerInterpolatorSuite extends ScalaCheckSuite {
   test("The pointer string interpolater should parse valid absolute JSON pointers") {
     val inputs = List("", "/foo", "/foo/0", "/", "/a~1b", "/c%d", "/e^f", "/g|h", "/i\\j", "/k\"l", "/ ", "/m~0n")
     val values = List(
@@ -59,35 +58,5 @@ class PointerInterpolatorSuite extends ScalaCheckSuite {
 
       pointer"/foo/$v/bar" == expected
     }
-  }
-
-  test("The pointer string interpolater should fail to compile on invalid literals") {
-    assertNoDiff(
-      compileErrors("pointer\"foo\""),
-      """|error: Invalid JSON Pointer in interpolated string
-         |pointer"foo"
-         |^
-         |""".stripMargin
-    )
-  }
-
-  test("The pointer string interpolater should fail with an interpolated relative distance") {
-    assertNoDiff(
-      compileErrors("""{val x = 1; pointer"$x/"}"""),
-      """|error: Invalid JSON Pointer in interpolated string
-         |{val x = 1; pointer"$x/"}
-         |            ^
-         |""".stripMargin
-    )
-  }
-
-  test("The pointer string interpolater should fail with an empty interpolated relative distance") {
-    assertNoDiff(
-      compileErrors("""{val x = ""; pointer"$x/"}"""),
-      """|error: Invalid JSON Pointer in interpolated string
-         |{val x = ""; pointer"$x/"}
-         |             ^
-         |""".stripMargin
-    )
   }
 }
