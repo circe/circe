6bbb7e73a529916faec89f1455d11356af03ea64
Kai(luo) Wang <kailuo.wang@gmail.com>
2023-03-02 17:56:35 -0500

    fixed 2096 (#2098)
    Co-authored-by: Darren Gibson <zarthross@users.noreply.github.com>

--- modules/core/shared/src/main/scala-3/io/circe/derivation/ConfiguredEncoder.scala
+++ modules/core/shared/src/main/scala-3/io/circe/derivation/ConfiguredEncoder.scala
@@ -8,8 +8,9 @@ trait ConfiguredEncoder[A](using conf: Configuration) extends Encoder.AsObject[A
   lazy val elemLabels: List[String]
   lazy val elemEncoders: List[Encoder[?]]
 
-  final def encodeElemAt(index: Int, elem: Any, transformName: String => String): (String, Json) =
+  final def encodeElemAt(index: Int, elem: Any, transformName: String => String): (String, Json) = {
     (transformName(elemLabels(index)), elemEncoders(index).asInstanceOf[Encoder[Any]].apply(elem))
+  }
 
   final def encodeProduct(a: A): JsonObject =
     val product = a.asInstanceOf[Product]
@@ -20,16 +21,30 @@ trait ConfiguredEncoder[A](using conf: Configuration) extends Encoder.AsObject[A
 
   final def encodeSum(index: Int, a: A): JsonObject =
     val (constructorName, json) = encodeElemAt(index, a, conf.transformConstructorNames)
+
     conf.discriminator match
-      case None => JsonObject.singleton(constructorName, json)
       case Some(discriminator) =>
-        json.asObject.getOrElse(JsonObject.empty).add(discriminator, Json.fromString(constructorName))
+        val jo = json.asObject.getOrElse(JsonObject.empty)
+        val elemIsSum = elemEncoders(index) match {
+          case ce: ConfiguredEncoder[?] with SumOrProduct => ce.isSum
+          case _                                          => jo.contains(discriminator)
+        }
+        if (elemIsSum)
+          jo
+        else jo.add(discriminator, Json.fromString(constructorName)) // only add discriminator if elem is a Product
+
+      case None => JsonObject.singleton(constructorName, json)
 
 object ConfiguredEncoder:
   inline final def derived[A](using conf: Configuration)(using inline mirror: Mirror.Of[A]): ConfiguredEncoder[A] =
-    new ConfiguredEncoder[A]:
+    new ConfiguredEncoder[A] with SumOrProduct:
       lazy val elemLabels: List[String] = summonLabels[mirror.MirroredElemLabels]
       lazy val elemEncoders: List[Encoder[?]] = summonEncoders[mirror.MirroredElemTypes]
+
+      lazy val isSum: Boolean =
+        inline mirror match
+          case _: Mirror.ProductOf[A] => false
+          case _: Mirror.SumOf[A]     => true
 
       final def encodeObject(a: A): JsonObject =
         inline mirror match
