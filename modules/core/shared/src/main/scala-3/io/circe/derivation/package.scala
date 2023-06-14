874c451d6d19a8af0d074061dfa48ad740bfe293
Kamil Podsiadlo <kamilpodsiadlo44@gmail.com>
2023-06-14 13:15:39 +0200

    feat: allow to derive enumeration codec for nested sealed hierarchies

--- modules/core/shared/src/main/scala-3/io/circe/derivation/package.scala
+++ modules/core/shared/src/main/scala-3/io/circe/derivation/package.scala
@@ -3,11 +3,33 @@ package io.circe.derivation
 import scala.compiletime.{ codeOf, constValue, erasedValue, error, summonFrom, summonInline }
 import scala.deriving.Mirror
 import io.circe.{ Codec, Decoder, Encoder }
+import scala.reflect.TypeTest
 
 private[circe] inline final def summonLabels[T <: Tuple]: List[String] =
   inline erasedValue[T] match
     case _: EmptyTuple => Nil
     case _: (t *: ts)  => constValue[t].asInstanceOf[String] :: summonLabels[ts]
+
+private[circe] inline final def summonLabelsRecursively[T <: Tuple]: List[String] =
+  inline erasedValue[T] match
+    case _: EmptyTuple => Nil
+    case _: (t *: ts) =>
+      inline summonInline[Mirror.Of[t]] match
+        case mirror: Mirror.SumOf[t] =>
+          Predef.println(s"sum of: ${constValue[mirror.MirroredLabel]}")
+          summonLabelsRecursively[mirror.MirroredElemTypes] ::: summonLabelsRecursively[ts]
+        case mirror: Mirror.ProductOf[t] =>
+          Predef.println(s"product of: ${constValue[mirror.MirroredLabel]}")
+          constValue[mirror.MirroredLabel] :: summonLabelsRecursively[ts]
+
+private inline def summonTypeTestRecursively[T <: Tuple, A]: List[TypeTest[A, ?]] = inline erasedValue[T] match
+    case _: (t *: ts)  =>
+        inline summonInline[Mirror.Of[t]] match
+            case mirror: Mirror.SumOf[t]     =>
+                summonTypeTestRecursively[mirror.MirroredElemTypes, A] ::: summonTypeTestRecursively[ts, A]
+            case mirror: Mirror.ProductOf[t] =>
+                summon[TypeTest[A, t]] :: summonTypeTestRecursively[ts, A]
+    case _: EmptyTuple => Nil
 
 private[circe] inline final def summonEncoders[T <: Tuple](using Configuration): List[Encoder[_]] =
   inline erasedValue[T] match
@@ -36,6 +58,9 @@ private[circe] inline def summonSingletonCases[T <: Tuple, A](inline typeName: A
     case _: EmptyTuple => Nil
     case _: (h *: t) =>
       inline summonInline[Mirror.Of[h]] match
-        case m: Mirror.Singleton => m.fromProduct(EmptyTuple).asInstanceOf[A] :: summonSingletonCases[t, A](typeName)
+        case m: Mirror.Singleton => 
+          m.fromProduct(EmptyTuple).asInstanceOf[A] :: summonSingletonCases[t, A](typeName)
+        case m: Mirror.SumOf[h]  => 
+          summonSingletonCases[m.MirroredElemTypes, A](constValue[m.MirroredLabel]) ::: summonSingletonCases[t, A](typeName)
         case m: Mirror =>
           error("Enum " + codeOf(typeName) + " contains non singleton case " + codeOf(constValue[m.MirroredLabel]))
