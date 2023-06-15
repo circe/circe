d00650c0893dcdd0e6f25eb79d1f406b1fc9b62c
cacapouh <yoshikti75@gmail.com>
2023-06-15 22:31:38 +0900

    Revert ConfiguredCodec#derive

--- modules/core/shared/src/main/scala-3/io/circe/derivation/ConfiguredCodec.scala
+++ modules/core/shared/src/main/scala-3/io/circe/derivation/ConfiguredCodec.scala
@@ -4,7 +4,18 @@ import scala.deriving.Mirror
 import io.circe.{ Codec, Decoder, HCursor, JsonObject }
 
 trait ConfiguredCodec[A] extends Codec.AsObject[A]
+
 object ConfiguredCodec:
+  inline final def derive[A: Mirror.Of](
+    transformMemberNames: String => String = Configuration.default.transformMemberNames,
+    transformConstructorNames: String => String = Configuration.default.transformConstructorNames,
+    useDefaults: Boolean = Configuration.default.useDefaults,
+    discriminator: Option[String] = Configuration.default.discriminator,
+    strictDecoding: Boolean = Configuration.default.strictDecoding
+  ): ConfiguredCodec[A] =
+    derived[A](using
+      Configuration(transformMemberNames, transformConstructorNames, useDefaults, discriminator, strictDecoding)
+    )
   inline final def derived[A](using Configuration)(using inline mirror: Mirror.Of[A]): ConfiguredCodec[A] =
     val encoder = ConfiguredEncoder.derived[A]
     val decoder = ConfiguredDecoder.derived[A]
