0872b9888165a6f95bbf3614d517b44d58fb3b87
Erlend Hamnaberg <erlend@hamnaberg.net>
2023-07-27 14:53:34 +0200

    run prePR

--- modules/tests/shared/src/test/scala-2/io/circe/FromProductSuite.scala
+++ modules/tests/shared/src/test/scala-2/io/circe/FromProductSuite.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 circe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.circe
 
 import syntax._
@@ -8,21 +24,7 @@ class FromProductSuite extends CirceMunitSuite {
   test("encode correctly Example(id: Int, value: String)") {
     case class Example(id: Int, value: String)
 
-    implicit val encoder: Encoder[Example] = Encoder.forProduct2("id", "value")(Example.unapply(_).get)
-    implicit val decoder: Decoder[Example] = Decoder.forProduct2("id", "value")(Example.apply)
-
-    val example = Example(1, "hello")
-
-    val encoded = example.asJson
-    val decoded = encoded.as[Example]
-
-    assertEquals(decoded, Right(example))
-  }
-
-  test("encode correctly Example(id: Int, value: String) tupled") {
-    case class Example(id: Int, value: String)
-
-    implicit val encoder: Encoder[Example] = Encoder.forTupledProduct2("id", "value")(Example.unapply(_).get)
+    implicit val encoder: Encoder[Example] = Encoder.forTupleProduct2("id", "value")(Example.unapply(_).get)
     implicit val decoder: Decoder[Example] = Decoder.forProduct2("id", "value")(Example.apply)
 
     val example = Example(1, "hello")
@@ -36,20 +38,7 @@ class FromProductSuite extends CirceMunitSuite {
   test("codec encode/decode correctly Example(id: Int, value: String)") {
     case class Example(id: Int, value: String)
 
-    implicit val encoder: Codec[Example] = Codec.forProduct2("id", "value")(Example.apply)(Example.unapply(_).get)
-
-    val example = Example(1, "hello")
-
-    val encoded = example.asJson
-    val decoded = encoded.as[Example]
-
-    assertEquals(decoded, Right(example))
-  }
-
-  test("codec encode/decode correctly Example(id: Int, value: String) tupled") {
-    case class Example(id: Int, value: String)
-
-    implicit val encoder: Codec[Example] = Codec.forTupledProduct2("id", "value")(Example.apply)(Example.unapply(_).get)
+    implicit val encoder: Codec[Example] = Codec.forTupleProduct2("id", "value")(Example.apply)(Example.unapply(_).get)
 
     val example = Example(1, "hello")
 
