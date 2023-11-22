3eda4fe870784d37505aa5e9e0f6644b096b2199
Erlend Hamnaberg <erlend@hamnaberg.net>
2023-11-22 20:16:08 +0100

    Introduce forTypedProduct to work better with Scala 3 (#1998)
    * Revert #630 and restore tupleN
    
    When using Scala 3 we tuples does not always implement ProductN.
    Meaning that we cannot use generic tuples, but only "named".
    
    For instance while using the code
    
    ```Scala
    case class Person(name: String, age: Int)
    
    object Person:
     given Encoder.AsObject[Person] = Encoder.forProduct2("name",
    "age")(Tuple.fromProductTyped[Person])
    ```
    
    is not possible, since this type of tuple does not implement Product2.
    
    The only way we can do this in Scala3 is by having an instance like
    this:
    
    ```Scala
     given Encoder.AsObject[Person] = Encoder.forProduct2("name", "age")(x
    => (x.name, x.age))
    ```
    
    which I think at least is less useful and more error prone than the
    version above.
    
    * Add tests for both Scala 2 and 3
    
    * overloaded version with dummy implicit
    
    * ScalafmtSbt
    
    * Rename overload to forTupleProduct
    
    Deprecate the old version
    
    Bikeshedding of new name is needed.
    forNamedProduct, ofProduct are other suggestions.
    
    * add missing header
    
    * run prePR
    
    * Rename to forProductTyped
    
    Make forProductTyped implicit values call by name on 2.13 and 3
    
    Allows us to write recursive instances of Codecs and Encoders.
    
    * Cache instances when call by name
    
    * fix output type
    
    * added forTypedProduct to ProductDecoders to keep in sync with encoders
    
    No changes to signature
    
    ---------
    
    Co-authored-by: Darren Gibson <zarthross@users.noreply.github.com>
    Co-authored-by: Zach McCoy <280974+zmccoy@users.noreply.github.com>

--- modules/tests/shared/src/main/scala/io/circe/tests/examples/package.scala
+++ modules/tests/shared/src/main/scala/io/circe/tests/examples/package.scala
@@ -132,7 +132,7 @@ package examples {
     )
 
     val decodeBam: Decoder[Bam] = Decoder.forProduct2("w", "d")(Bam.apply)(Wub.decodeWub, implicitly)
-    val encodeBam: Encoder[Bam] = Encoder.forProduct2[Bam, Wub, Double]("w", "d") {
+    val encodeBam: Encoder[Bam] = Encoder.forTypedProduct2[Bam, Wub, Double]("w", "d") {
       case Bam(w, d) => (w, d)
     }(Wub.encodeWub, implicitly)
   }
